59e4226530c18fa817ffb4c2292a27da04b3fcc1
Gabriel Taillon <ga.taillon@gmail.com>
2019-06-30 02:06:48 +0900

    Understanding of constructors, default parameters, obhject creation, as well as separating declaration and implementation.

--- classes.cpp
+++ classes.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <vector>
 #include <bits/stdc++.h> 
+#include "classes.h"
 using namespace std;
 
 std::vector<std::string> unit_stats = {"HP", "Str", "Mag", "Skill", "Speed", "Luck", "Def", "Res", "Con"};
@@ -12,50 +13,52 @@ std::vector<std::string> statuses = {"healthy", "sleep", "poison", "stone", "ber
 std::vector<std::string> unit_classes = {"Lord", "Prince", "Princess", "Knight", "Cavalier", "Pegasus Knight", "Wyvern knight"};
 int equipment_slots = 7;
 
-class character {
-    public:
-        std::vector<int> stats, stat_bonus, wpn_lvls, equipment, position;
-        bool mounted, flying;
-        int equipped, id, unit_class;
-        std::vector<std::string> weapons;
-        
-        character(int test=0) {
-            std::vector<int> stats = std::vector<int>(unit_stats.size());
+character::character(std::vector<int> in_stats = {1,1},
+                    std::vector<int> in_stat_bonus = std::vector<int>(1),
+                    std::vector<int> in_wpn_exp = std::vector<int>(1),
+                    std::vector<int> in_equipment = std::vector<int>(1),
+                    std::vector<int> in_position = std::vector<int>(1))
+{
+    stats = in_stats;
+    stat_bonus = in_stat_bonus;
+    wpn_exp = in_wpn_exp;
+    equipment = in_equipment;
+    position = in_position;
+}
+   
+int character::combat_damage(){
+    int damage = stats[1];
+    // cout << weapons[equipped] << "\n";
+    return damage;
+}
+main() {
+    std::cout << "TESTING THIS BITCH\n";
+    character Marth;
+    Marth.stats = {18,}
             std::vector<int> stat_bonus = std::vector<int>(unit_stats.size());
             std::vector<int> wpn_exp = std::vector<int>(wpn_types.size());
             std::vector<int> equipment = std::vector<int>(equipment_slots);
             std::vector<int> position = std::vector<int>(3);
             std::string name;
+    // Marth.stats = {18, 2};
+    std::cout << Marth.stats[0] << "\n";
+    // std::cout << Marth.stat_bonus[0] << "\n";
+    return 0;
+}
 
-        };        
-        int combat_damage(void){
-            int damage = stats[1] + weapons[equipped];
-            // cout << stats[0] + weapons[equipped] << "\n";
-            return damage;
-        }
-        // int combat_avoid();
-        // int combat_crit(); 
-        
-        // if ever write .dll or .so, use char in dll/so exposed functions.
-        // char name = 
-        /* Stats order:
-            
-        */    
-};
+// class weapon {
+    // public:
+        // std::vector<int> stats = std::vector<int>(equipment_slots);
+        // std::vector<int> stat_bonus = std::vector<int>(unit_stats.size());
+        // std::string name;
+        // std::vector<std::string> effective;
+        // int id;
+// };
 
-class weapon {
-    public:
-        std::vector<int> stats = std::vector<int>(equipment_slots);
-        std::vector<int> stat_bonus = std::vector<int>(unit_stats.size());
-        std::string name;
-        std::vector<std::string> effective;
-        int id;
-};
-
-class map {
-    public:
-        std::vector<int> map_size = {256, 256};
-        // std::vector<int> tiles(map_size[0], map_size[1]);
+// class map {
+    // public:
+        // std::vector<int> map_size = {256, 256};
+        // // std::vector<int> tiles(map_size[0], map_size[1]);
         
 
-};
+// };
--- classes.h
+++ classes.h
@@ -1,13 +1,18 @@
-#ifndef classes_H
+#pragma once
 #define classes_H
 
 class character {
     public:
-        std::vector<int> stats, stat_bonus, wpn_lvls, equipment, position;
+        std::vector<int> stats, stat_bonus, wpn_exp, equipment, position;
         bool mounted, flying;
-        int equipped, id, unit_class;
+        int equipped, id, unit_class, level, promotion;
         std::vector<std::string> weapons;
         int combat_damage(void); 
+        character(std::vector<int> in_stats,
+                    std::vector<int> in_stat_bonus,
+                    std::vector<int> in_wpn_exp,
+                    std::vector<int> in_equipment,
+                    std::vector<int> in_position);
 };
 
 class weapon {
