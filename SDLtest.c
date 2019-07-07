df4714aa75b20f4f4bfd6fe82522e7822a744977
Gabriel Taillon <ga.taillon@gmail.com>
2019-07-07 20:52:12 +0900

    File structure with nmake working for now. Think about making makefiles for other platforms or building myself for linux.

--- SDLtest.c
+++
@@ -1,9 +0,0 @@
-#include "SDL2/SDL.h" 
-#include <stdio.h>
-
-int main( int argc, char* args[] ) {
-  SDL_Init( SDL_INIT_EVERYTHING ); //Start SDL
-  SDL_Quit(); //Quit SDL
-  printf("allo\n");
-  return 0;
-}
