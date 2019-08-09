77c05b2b0d56f60e816f879a7832e62b5796f3e0
Gabriel Taillon <ga.taillon@gmail.com>
2019-08-10 00:37:21 +0900

    since unit read is acting fucking weird again, ill try with a stream

--- src/main.cpp
+++ src/main.cpp
@@ -378,18 +378,18 @@ main() {
     
     all_units["Sheeda_test"].read("units.txt", 0);
     // all_weapons["Sheeda_test"].read("weapons.txt", 20);
-    // printf("%s \n", all_units["Sheeda_test"].name);
-    // printf("%s \n", all_units["Sheeda_test"].type);
-    // printf("%d \n", all_units["Sheeda_test"].stats_base[0]);
-    // printf("%d \n", all_units["Sheeda_test"].stats_base[1]);
-    // printf("%d \n", all_units["Sheeda_test"].stats_base[2]);
-    // printf("%d \n", all_units["Sheeda_test"].growths[0]);
-    // printf("%d \n", all_units["Sheeda_test"].stats[0]);
-    // printf("%d \n", all_units["Sheeda_test"].get_equipped()[0]);
-    // printf("%s \n", all_units["Sheeda_test"].equipment[0].name);
-    // cout << all_units["Sheeda_test"].equipment[0].name << endl;
-    // printf("%s \n", all_units["Sheeda_test"].equipment[1].name);
-    // printf("%s \n", all_units["Sheeda_test"].lovers[0]);
+    printf("%s \n", all_units["Sheeda_test"].name);
+    printf("%s \n", all_units["Sheeda_test"].type);
+    printf("%d \n", all_units["Sheeda_test"].stats_base[0]);
+    printf("%d \n", all_units["Sheeda_test"].stats_base[1]);
+    printf("%d \n", all_units["Sheeda_test"].stats_base[2]);
+    printf("%d \n", all_units["Sheeda_test"].growths[0]);
+    printf("%d \n", all_units["Sheeda_test"].stats[0]);
+    printf("%d \n", all_units["Sheeda_test"].get_equipped()[0]);
+    printf("%s \n", all_units["Sheeda_test"].equipment[0].name);
+    cout << all_units["Sheeda_test"].equipment[0].name << endl;
+    printf("%s \n", all_units["Sheeda_test"].equipment[1].name);
+    printf("%s \n", all_units["Sheeda_test"].lovers[0]);
 
     
 }
