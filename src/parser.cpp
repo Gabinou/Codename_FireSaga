99bd7f21fb007c9c98af580e5bdcf69f30c2297c
Gabriel Taillon <ga.taillon@gmail.com>
2019-10-19 22:19:03 +0900

    c++ cannot do multiple return easily.

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <stdio.h>
+#include <tuple>
 
 int findinchar(const char * string, const char * search) {
     puts(string);
@@ -19,33 +20,49 @@ int findinchar(const char * string, const char * search) {
 /// \brief Find start and end position between first 'open' and last 'close'.
 /// Intended to find start and end position between a bunch of nested bracket pairs. 
 /// I.E. Should find these ->{ foo{ bar}}<-. Or these: ->{ }<- foo{ bar}}. 
-int between(FILE * fp, const char *open, const char *close, const char *interrupt = "@") {
+std::tuple<fpos_t, fpos_t> between(FILE * fp, const char *open, const char *close, const char *interrupt = "@") {
     char single_char;
     int open_num = 0;
     int close_num = 0;
-    current_pos = ftell(fp);
     fpos_t fopen_pos;
     fpos_t fclose_pos;
     while((!feof(fp)) && (single_char != * interrupt) && ((open_num!=close_num)) || (open_num == 0)) {
-        // printf("%d,%d\n", open_num, close_num);
         single_char = fgetc(fp);
         if (single_char == * open) {
             if (open_num == 0) {
-                open_pos = ftell(fp);
                 fgetpos(fp, &fopen_pos);
                 }
             open_num++;
         }
         if (single_char == * close) {close_num++;}
-        printf("%c", single_char);
+        // printf("%c", single_char);
     }
-    close_pos = ftell(fp);
     fgetpos(fp, &fclose_pos);
-    return(fopen_pos, fclose_pos);
-}
-
-char fgetsuntil(FILE * fp, const char *open, const char *close, const char *interrupt = "@", const char * filelength = "short"){
     
+    rewind(fp);
+    char line_c[100];
+    fsetpos(fp, &fopen_pos);
+    fgets(line_c, sizeof(line_c), fp);
+    printf("HERE THOUGH2\n");
+    printf("%s\n", line_c);
+    return std::make_tuple(fopen_pos, fclose_pos);
+}
+/// \var int until
+/// \brief Find position in file of next 'until' character.
+char until(FILE * fp, const char * until = ",",  const char * interrupt = "@"){
+    char single_char;
+    char buffer[255];
+    int i = 0;
+    while((!feof(fp)) && (single_char != * until)){
+        single_char = fgetc(fp);
+        printf("%c\n", single_char);
+        buffer[i] = single_char;
+        i++;
+    }
+    buffer[i] = '\0';
+    printf("buffer");
+    printf("%s\n", buffer);                
+    return(single_char);
 }
 
 
@@ -58,7 +75,9 @@ void read(const char *filename) {
     const char* open = "{";
     const char* close = "}";
     unsigned int curLine = 0;
-
+    fpos_t fopen_pos;
+    fpos_t fclose_pos;
+    
     fpos_t pos; // for arbitrarily long files
     std::string line_str;
     fp = fopen(filename, "r");
@@ -68,8 +87,24 @@ void read(const char *filename) {
         while(!feof(fp)) {
             single_char = fgetc(fp);
             if (single_char == * elem) {
-                printf("%c\n", single_char);
-                between(fp, open, close);
+                // printf("%c\n", single_char);
+                // printf("until\n");
+                rewind(fp);
+                std::tie(fopen_pos, fclose_pos) = between(fp, open, close);
+                rewind(fp);
+                fsetpos(fp, &fopen_pos);
+                fgets(line_c, sizeof(line_c), fp);
+                // fgets(line_c, sizeof(line_c), fp);
+                // fgets(line_c, sizeof(line_c), fp);
+                // fgets(line_c, sizeof(line_c), fp);
+                // fgets(line_c, sizeof(line_c), fp);
+                // fgets(line_c, sizeof(line_c), fp);
+                // fgets(line_c, sizeof(line_c), fp);<
+                // fgets(line_c, sizeof(line_c), fp);
+                printf("HERE THOUGH\n");
+                printf("%s\n", line_c);
+
+                // until(fp, ",");
                 break;
                 // printf("%d \n", findinchar(line_c, elem));
                 // fgetpos(fp,&pos);
