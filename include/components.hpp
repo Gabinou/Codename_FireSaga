3ff61a83482f92ca74e6e5744b2b965c1543df49
Gabriel Taillon <ga.taillon@gmail.com>
2019-10-22 01:10:27 +0900

    Made ECS and components headers.

---
+++ include/components.hpp
@@ -0,0 +1,32 @@
+#ifndef COMPONENTS_HPP
+#define COMPONENTS_HPP
+
+#include "ECS.hpp"
+
+class PositionComponent : public Component {
+    private:
+        int xpos;
+        int ypos;
+    public:
+        int x(){return(xpos);};
+        int y(){return(ypos);};
+        
+        void setPos(int x, int y) {
+            xpos = x;
+            ypos = y;
+        }
+        
+        void init() override {
+            xpos = 0;
+            ypos = 0;
+        }
+        
+        void update override {
+            xpos++;
+            ypos++;
+        }
+    
+} 
+
+
+#endif /* COMPONENTS_HPP */
