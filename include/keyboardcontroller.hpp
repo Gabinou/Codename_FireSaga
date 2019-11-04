439b3d3805ba29f9d9ae2a7b52ea42a76779cd76
Gabriel Taillon <ga.taillon@gmail.com>
2019-11-05 00:19:54 +0900

    Trying a different strategy to handle inputs.

--- include/keyboardcontroller.hpp
+++ include/keyboardcontroller.hpp
@@ -29,68 +29,74 @@ class KeyboardController : public Component, Tilesize, KeyboardInputMapping {
     }
 
     void update() override {
-        std::string currentcombination;
+        std::string currentcombination = "";
         if (Game::event.type == SDL_KEYDOWN){
-            currentcombination = "";
+            // currentcombination = "";
             static int * currenttilesize = (getTilesize());
             if (std::find(this->moveright.begin(), this->moveright.end(), Game::event.key.keysym.sym) != this->moveright.end()){
                 // positioncomponent->position.add(Vector2D(currenttilesize[0],0));
-                currentcombination += "right";
+                printf("right");
+                currentcombination.append("right");
             }
             if (std::find(this->moveleft.begin(), this->moveleft.end(), Game::event.key.keysym.sym) != this->moveleft.end()){
                 // positioncomponent->position.add(Vector2D(-currenttilesize[0],0));
-                currentcombination += "left";
-            }
+                printf("left");
+                currentcombination.append("left");
+            } 
             if (std::find(this->movedown.begin(), this->movedown.end(), Game::event.key.keysym.sym) != this->movedown.end()){
+                printf("down");
                 // positioncomponent->position.add(Vector2D(0,currenttilesize[1]));
-                currentcombination += "down";
+                currentcombination.append("down");
             } 
             if (std::find(this->moveup.begin(), this->moveup.end(), Game::event.key.keysym.sym) != this->moveup.end()){
                 // positioncomponent->position.add(Vector2D(0,-currenttilesize[1]));
-                currentcombination += "up";
+                printf("up");
+                currentcombination.append("up");
             }
+            printf("\n");
+            printf("Downed: %s\n", currentcombination.c_str());
             if (currentcombination.compare(lastpressed.combination) == 0) {
                 lastpressed.activeframes++;
             } else {
                 lastpressed.combination = currentcombination;
                 lastpressed.activeframes = 0;
             }
-            if ((lastpressed.combination.compare("right") == 0) && (lastpressed.activeframes == 0)) {
+            if ((lastpressed.combination.compare("right") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
                 positioncomponent->position.add(Vector2D(currenttilesize[0],0));
             }
-            if ((lastpressed.combination.compare("rightup") == 0) && (lastpressed.activeframes == 0)) {
+            if ((lastpressed.combination.compare("rightup") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
                 positioncomponent->position.add(Vector2D(currenttilesize[0],-currenttilesize[1]));
             }
-            if ((lastpressed.combination.compare("left") == 0) && (lastpressed.activeframes == 0)) {
+            if ((lastpressed.combination.compare("rightdown") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
+                positioncomponent->position.add(Vector2D(currenttilesize[0],currenttilesize[1]));
+            }
+            if ((lastpressed.combination.compare("left") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
                 positioncomponent->position.add(Vector2D(-currenttilesize[0],0));
             }
-            if ((lastpressed.combination.compare("up") == 0) && (lastpressed.activeframes == 0)) {
+            if ((lastpressed.combination.compare("leftup") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
+                positioncomponent->position.add(Vector2D(-currenttilesize[0],-currenttilesize[1]));
+            }
+            if ((lastpressed.combination.compare("leftdown") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
+                positioncomponent->position.add(Vector2D(-currenttilesize[0],currenttilesize[1]));
+            }
+            if ((lastpressed.combination.compare("up") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
                 positioncomponent->position.add(Vector2D(0, -currenttilesize[1]));
             }
-            if ((lastpressed.combination.compare("down") == 0) && (lastpressed.activeframes == 0)) {
+            if ((lastpressed.combination.compare("down") == 0) && ((lastpressed.activeframes == 0) || (lastpressed.activeframes % 5 == 0))) {
                 positioncomponent->position.add(Vector2D(0, currenttilesize[1]));
             }
         }
-        if (Game::event.type == SDL_KEYUP){ 
-            currentcombination = "";
-            if (std::find(this->moveright.begin(), this->moveright.end(), Game::event.key.keysym.sym) != this->moveright.end()){
-                currentcombination += "right";
-            }
-            if (std::find(this->moveleft.begin(), this->moveleft.end(), Game::event.key.keysym.sym) != this->moveleft.end()){
-                currentcombination += "left";
-            }
-            if (std::find(this->movedown.begin(), this->movedown.end(), Game::event.key.keysym.sym) != this->movedown.end()){
-                currentcombination += "down";
-            } 
-            if (std::find(this->moveup.begin(), this->moveup.end(), Game::event.key.keysym.sym) != this->moveup.end()){
-                currentcombination += "up";
-            }
+        // if (Game::event.type == SDL_KEYUP){ 
+            // currentcombination = "";
+            // if (std::find(this->moveright.begin(), this->moveright.end(), Game::event.key.keysym.sym) != this->moveright.end()){
+                // // printf("Last Pressed: %s\n", lastpressed.combination.c_str());
+                // // if (lastpressed.combination.find("right")>0) {
+                    // // lastpressed.combination.erase(lastpressed.combination.find("right"), 5);
+                // // }
+            // }
+
             
-            if ((lastpressed.combination.compare(currentcombination) == 0)) {
-                lastpressed.activeframes = 0;
-            }
-            
-        }
+        // }
     }
     
 };
