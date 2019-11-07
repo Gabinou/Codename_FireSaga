c038636f28c77eff47612da589d105120ff95c87
Gabriel Taillon <ga.taillon@gmail.com>
2019-11-07 23:36:50 +0900

    Found my more elegant solution.

--- ECS/spritecomponent.hpp
+++ ECS/spritecomponent.hpp
@@ -9,6 +9,10 @@
 #include "texturemanager.hpp"
 #include "SDL2/SDL.h"
 
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
+
 class SpriteComponent : public Component{
     
     private:
@@ -53,13 +57,21 @@ class SpriteComponent : public Component{
         void update() override {
             objectivepos.x = (int)positioncomponent->getPos().x * currenttilesize[0];
             objectivepos.y = (int)positioncomponent->getPos().y * currenttilesize[1];
-            printf("ObjectivePos: %d, %d\n", objectivepos.x, objectivepos.y);
-            printf("SlidePos    : %d, %d\n", slidepos.x, slidepos.y);
-            if (objectivepos.x > slidepos.x){
-                slidepos.x += std::max((objectivepos.x - slidepos.x)/2, 1);
-            } else if  (objectivepos.x < slidepos.x){
-                slidepos.x += std::min((objectivepos.x - slidepos.x)/2, - 1);
+            // printf("ObjectivePos: %d, %d\n", objectivepos.x, objectivepos.y);
+            // printf("SlidePos    : %d, %d\n", slidepos.x, slidepos.y);
+            // if (objectivepos.x > slidepos.x){
+                // slidepos.x += std::max((objectivepos.x - slidepos.x)/2, 1);
+            // } else if  (objectivepos.x < slidepos.x){
+                // slidepos.x += std::min((objectivepos.x - slidepos.x)/2, - 1);
+            // }
+            
+            if (objectivepos.x != slidepos.x) {
+                int sign = sgn(objectivepos.x - slidepos.x);
+                int distx = objectivepos.x - slidepos.x;
+                slidepos.x += sign*std::max(sign*distx/2, 1);
+                // Could Use absolute value here instead of sign*distx, But I like the elegance of calling upon only one extra function
             }
+            
             if (objectivepos.y > slidepos.y){
                 slidepos.y += std::max((objectivepos.y - slidepos.y)/2, 1);
             } else if  (objectivepos.y < slidepos.y){
