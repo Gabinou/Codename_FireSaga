ca1fa7a1ef5b11fed5ea3cec7a5b18c760d3122c
Gabriel Taillon <ga.taillon@gmail.com>
2019-11-21 00:53:10 +0900

    Adds more options for setters and constructors for Position component. Also, trying to make variable names uniform.

--- ECS/cursorcomponent.hpp
+++ ECS/cursorcomponent.hpp
@@ -12,31 +12,9 @@ class CursorComponent : public SpriteComponent {
         KeyboardController * keyboardcontroller;
         float slidefactors[2] = {2, 1.025};
         int slide_int = 0;
-        unsigned int bounds[4] = {0, 255, 0, 255}; //xmin,xmax,ymin,ymax
 
     public:
         using SpriteComponent::SpriteComponent; //inherits all constructors
-
-        void setBounds(unsigned int xmin, unsigned int xmax, unsigned int ymin, unsigned int ymax) {
-            bounds[0] = xmin;
-            bounds[1] = xmax;
-            bounds[2] = ymin;
-            bounds[3] = ymax;
-        }
-
-        void setBounds(unsigned int in_bounds[4]) {
-            bounds[0] = in_bounds[0];
-            bounds[1] = in_bounds[1];
-            bounds[2] = in_bounds[2];
-            bounds[3] = in_bounds[3];
-        }
-
-        void setBounds(std::vector<unsigned int> in_bounds) {
-            bounds[0] = in_bounds[0];
-            bounds[1] = in_bounds[1];
-            bounds[2] = in_bounds[2];
-            bounds[3] = in_bounds[3];
-        }
 
         void init() override {
             printf("init\n");
--- ECS/positioncomponent.hpp
+++ ECS/positioncomponent.hpp
@@ -11,27 +11,67 @@ class PositionComponent : public Component {
     private:
         bool updatable = true;
         Vector2D position;
-    public:
+        unsigned int bounds[4] = {0, 255, 0, 255}; //xmin,xmax,ymin,ymax
 
+    public:
+        void setBounds(unsigned int xmin, unsigned int xmax, unsigned int ymin, unsigned int ymax) {
+            bounds[0] = xmin;
+            bounds[1] = xmax;
+            bounds[2] = ymin;
+            bounds[3] = ymax;
+        }
+
+        void setBounds(unsigned int in_bounds[4]) {
+            bounds[0] = in_bounds[0];
+            bounds[1] = in_bounds[1];
+            bounds[2] = in_bounds[2];
+            bounds[3] = in_bounds[3];
+        }
+
+        void setBounds(std::vector<unsigned int> in_bounds) {
+            bounds[0] = in_bounds[0];
+            bounds[1] = in_bounds[1];
+            bounds[2] = in_bounds[2];
+            bounds[3] = in_bounds[3];
+        }
 
         PositionComponent() {
             position.x = 0;
             position.y = 0;
         }
 
-        void setUpdatable(bool up_in) {
-            updatable = up_in;
+        void setUpdatable(bool in_updatable) {
+            updatable = in_updatable;
         }
 
-        PositionComponent(short unsigned int x, short unsigned int y) {
-            position.x = x;
-            position.y = y;
+        PositionComponent(unsigned int in_x, unsigned int in_y) {
+            position.x = in_x;
+            position.y = in_y;
         }
 
-        void setPos(short unsigned int x, short unsigned int y) {
+        PositionComponent(unsigned int in_x, unsigned int in_y,
+                          unsigned int xmin, unsigned int xmax,
+                          unsigned int ymin, unsigned int ymax)
+            : PositionComponent(in_x, in_y) {
+            setBounds(xmin, xmax, ymin, ymax);
+        }
+
+        PositionComponent(unsigned int in_x, unsigned int in_y,
+                          unsigned int in_bounds[4])
+            : PositionComponent(in_x, in_y) {
+            setBounds(in_bounds);
+        }
+
+        PositionComponent(unsigned int in_x, unsigned int in_y,
+                          std::vector<unsigned int> in_bounds)
+            : PositionComponent(in_x, in_y) {
+            setBounds(in_bounds);
+        }
+
+        void setPos(unsigned int in_x, unsigned int in_y) {
             if (updatable) {
-                position.x = x;
-                position.y = y;
+                position.x = in_x;
+                position.y = in_y;
             }
         }
 
