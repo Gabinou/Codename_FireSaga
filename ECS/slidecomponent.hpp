1ef44a7e2e38142fa4e507370a3a777b41aa121a
Gabriel Taillon <ga.taillon@gmail.com>
2019-11-24 00:03:35 +0900

    need some king of pathfinding algorithm. Anad for that I need to give tiles passable scores. But before that, define moving units using arrows.

--- ECS/slidecomponent.hpp
+++ ECS/slidecomponent.hpp
@@ -1,27 +1,35 @@
-#ifndef CURSORCOMPONENT_HPP
-#define CURSORCOMPONENT_HPP
+#ifndef SLIDECOMPONENT_HPP
+#define SLIDECOMPONENT_HPP
 
 #include "shared.hpp"
 #include "spritecomponent.hpp"
 #include "keyboardcontroller.hpp"
 #include "SDL2/SDL.h"
 
-class CursorComponent : public SpriteComponent {
+class SlideComponent : public SpriteComponent {
 
     private:
         KeyboardController * keyboardcontroller;
-        float slidefactors[2] = {2, 1.025};
-        int slide_int = 0;
+        float slidefactors[2] = {2, 1.025}; // for slide_type = "geometric"
+        int slideint = 0; // for slide_type = "geometric"
+        int slidespeed = 10;
+
+        std::string slidetype = "geometric";
 
     public:
         using SpriteComponent::SpriteComponent; //inherits all constructors
+        using SpriteComponent::SpriteComponent; //inherits all constructors
+
+        void setSlideType(std::string in_slidetype) {
+            slidetype = in_slidetype;
+        }
 
         void init() override {
             printf("init\n");
             SpriteComponent::init();
             keyboardcontroller = &entity->getComponent<KeyboardController>();
             keyboardcontroller->setTilesize(tilesize);
-            SpriteComponent::setSrcrect(64, 64); //Manually entered from cursor png size.
+            SpriteComponent::setSrcrect(64, 64); // Manually entered from cursor png size.
             SpriteComponent::setDestrect(tilesize[0] * 2, tilesize[1] * 2);
 
             slidepos.x = objectivepos.x = (int)positioncomponent->getPos().x * tilesize[0] - destrect.w / 4;
@@ -35,22 +43,23 @@ class CursorComponent : public SpriteComponent {
             objectivepos.y = (int)positioncomponent->getPos().y * (tilesize[1]) - destrect.h / 4;
             // printf("before: %d %d\n", slidepos.x, slidepos.y);
 
+            if (slidetype == "geometric") {
+                if (lastpressed.pressed_frames > 25) {
+                    slideint = 1;
+                }
 
-            if (lastpressed.pressed_frames > 25) {
-                slide_int = 1;
-            }
+                if (objectivepos.x != slidepos.x) {
+                    slidepos.x += geometricslide((objectivepos.x - slidepos.x), slidefactors[slideint]);
+                }
 
-            if (objectivepos.x != slidepos.x) {
-                slidepos.x += geometricslide((objectivepos.x - slidepos.x), slidefactors[slide_int]);
-            }
+                if (objectivepos.y != slidepos.y) {
+                    slidepos.y += geometricslide((objectivepos.y - slidepos.y), slidefactors[slideint]);
+                }
 
-            if (objectivepos.y != slidepos.y) {
-                slidepos.y += geometricslide((objectivepos.y - slidepos.y), slidefactors[slide_int]);
-            }
-
-            if ((objectivepos.x == slidepos.x) && (objectivepos.y == slidepos.y)) {
-                positioncomponent->setUpdatable(true);
-                slide_int = 0;
+                if ((objectivepos.x == slidepos.x) && (objectivepos.y == slidepos.y)) {
+                    positioncomponent->setUpdatable(true);
+                    slideint = 0;
+                }
             }
 
             // printf("after: %d %d\n", objectivepos.x, objectivepos.y);
@@ -63,4 +72,4 @@ class CursorComponent : public SpriteComponent {
 
 };
 
-#endif /* CURSORCOMPONENT_HPP */
+#endif /* SLIDECOMPONENT_HPP */
