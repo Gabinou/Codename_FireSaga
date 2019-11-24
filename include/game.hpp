cca760df152fc890591c8fb485b20f9aac4b0851
Gabriel Taillon <ga.taillon@gmail.com>
2019-11-25 00:48:14 +0900

    what to do about keyboardcontroller and game and entity creator...

--- include/game.hpp
+++ include/game.hpp
@@ -7,7 +7,7 @@
 #include <stdio.h>
 #include "vector2D.hpp"
 #include "shared.hpp"
-#include "ECS.hpp"
+// #include "ECS.hpp"
 
 struct InputMap {
     std::vector<SDL_Scancode> moveright{SDL_SCANCODE_RIGHT, SDL_SCANCODE_D, SDL_SCANCODE_L};
@@ -57,7 +57,7 @@ class Game {
 
     private:
         InputMap inputMap;
-        Manager & manager;
+        // Manager & manager;
         int fontsize;
         bool isRunning;
         int count;
