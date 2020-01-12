6c0339bdf222377ece21d33bf78a74a4d034c012
Gabriel Taillon <ga.taillon@gmail.com>
2020-01-12 12:55:04 +0900

    tiles is okay now I guess?

--- include/tile.hpp
+++ include/tile.hpp
@@ -5,16 +5,20 @@
 class Tile {
     private:
         Movement_cost cost;
+
         std::string name;
+
         struct {
             unsigned int hit;
             unsigned int dodge;
+            unsigned int def;
             unsigned int heal;
         } stats;
 
     public:
         Tile();
         ~Tile();
+        Tile(std::string in_name, Movement_cost in_cost, unsigned int * in_stats[3]);
 
 };
 
