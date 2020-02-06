3ebe78354778b1d2b3b26fc462870a981e1cdf8f
Gabriel Taillon <ga.taillon@gmail.com>
2020-02-06 19:51:52 +0900

    Forgot about the fact that functions can add themselves to the array of function pointers.

--- include/tile.hpp
+++ include/tile.hpp
@@ -30,8 +30,38 @@ class Tile {
 
 extern std::unordered_map<int, Tile> all_tiles;
 
+
 void baseTiles();
 std::unordered_map<int, Tile> baseTiles(std::vector<int> toload);
+
+std::vector<int> testTiles();
+std::vector<int> chap1Tiles();
+std::vector<int> gaiden1Tiles();
+std::vector<int> chap2Tiles();
+std::vector<int> chap3Tiles();
+std::vector<int> chap4Tiles();
+std::vector<int> chap5Tiles();
+std::vector<int> chap6Tiles();
+std::vector<int> chap7Tiles();
+std::vector<int> chap8Tiles();
+std::vector<int> chap9Tiles();
+std::vector<int> chap10Tiles();
+std::vector<int> chap11Tiles();
+std::vector<int> chap12Tiles();
+std::vector<int> chap13Tiles();
+std::vector<int> chap14Tiles();
+std::vector<int> chap15Tiles();
+std::vector<int> chap16Tiles();
+std::vector<int> chap17Tiles();
+std::vector<int> chap18Tiles();
+std::vector<int> chap19Tiles();
+std::vector<int> chap20Tiles();
+std::vector<int> chap21Tiles();
+std::vector<int> chap22Tiles();
+std::vector<int> chap23Tiles();
+std::vector<int> chap24Tiles();
+std::vector<int> chap25Tiles();
+
 
 extern std::vector<Map_enemy> (*chapTiles[25])();
 extern std::vector<Map_enemy> (*gaidenTiles[25])();
