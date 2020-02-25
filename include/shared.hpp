6ecca49669fdda06988b960a3f44013f56290ebd
Gabriel Taillon <ga.taillon@gmail.com>
2020-02-25 21:11:10 +0900

    enum for game state is SO MUCH BETTER hahaha.

--- include/shared.hpp
+++ include/shared.hpp
@@ -459,6 +459,35 @@ enum TILES {
 };
 }
 
+namespace GAME {
+namespace STATE {
+enum STATES {
+    PAUSE,
+    STATS,
+    MINIMAP,
+    MAP,
+    UNITMENU,
+    UNITMOVE,
+    CUTSCENE,
+    OPTIONS,
+    STARTMENU,
+    SOUNDROOM,
+    SAVES,
+    STORAGE,
+    FORGE,
+    BONUSEXP,
+    PLACEMENT,
+    PREPARATION,
+    CHOOSEUNITS,
+    ITEMS,
+    TRADE,
+    CONVERSTATION,
+    BATTLE,
+    ATTACK,
+};
+}
+}
+
 extern std::mt19937 mt;
 extern std::mt19937_64 mt_64;
 
