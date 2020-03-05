5ed7780acc89b6c123136ace9a2f9609c6762f71
Gabriel Taillon <ga.taillon@gmail.com>
2020-03-05 14:15:38 +0900

    Creates pathfinding.hpp and .cpp, migrating functions there.

--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -1,62 +1,5 @@
-// Code créé par Gabriel Taillon
-#include "shared.hpp"
-#include "game.hpp"
 
-Inventory_item convoy[200];
-
-std::vector<std::string> wpntype2str(short unsigned int in_type){
-    std::vector<std::string> types;
-    if ((in_type & WPN::TYPE::SWORD) > 0) {
-        types.push_back("Sword");
-    }
-    if ((in_type & WPN::TYPE::LANCE) > 0) {
-        types.push_back("Lance");
-    }
-    if ((in_type & WPN::TYPE::AXE) > 0) {
-        types.push_back("Axe");
-    }
-    if ((in_type & WPN::TYPE::BOW) > 0) {
-        types.push_back("Bow");
-    }
-    if ((in_type & WPN::TYPE::TRINKET) > 0) {
-        types.push_back("Trinket");
-    }
-    if ((in_type & WPN::TYPE::OFFHAND) > 0) {
-        types.push_back("Offhand");
-    }
-    if ((in_type & WPN::TYPE::ELEMENTAL) > 0) {
-        types.push_back("Magic");
-        // types.push_back("Elemental");
-    }
-    if ((in_type & WPN::TYPE::DEMONIC) > 0) {
-        types.push_back("Demonic");
-    }
-    if ((in_type & WPN::TYPE::ANGELIC) > 0) {
-        types.push_back("Angelic");
-    }
-    if ((in_type & WPN::TYPE::SHIELD) > 0) {
-        types.push_back("Shield");
-    }
-    if ((in_type & WPN::TYPE::STAFF) > 0) {
-        types.push_back("Staff");
-    }
-    return(types);
-}
-
-void writeText(int in_fontsize, int in_position[2], float in_sizefactor[2], std::string in_text, SDL_Color in_color, TTF_Font * in_font, SDL_Renderer * in_renderer) {
-    std::string text = "FPS";
-    SDL_Texture * texture = textToTexture(text, in_color, in_font);
-    SDL_Rect srcrect, destrect; // background always first?
-    srcrect.x = srcrect.y = 0;
-    destrect.x = in_position[0];
-    destrect.y = in_position[1];
-    srcrect.h = in_fontsize;
-    srcrect.w = text.length() * in_fontsize;
-    destrect.h = (int)in_fontsize * in_sizefactor[0];
-    destrect.w = (int)text.length() * in_fontsize * in_sizefactor[1];
-    SDL_RenderCopy(in_renderer, texture, &srcrect, &destrect);
-    SDL_RenderPresent(in_renderer);
-}
+#include "pathfinding.hpp"
 
 int h_manhattan (int start[], int end[]){
     // fast. Exact on a grid.
@@ -83,6 +26,7 @@ double h_euclidean (int start[], int end[]){
     return(std::sqrt(pow(start[0] - end[0], 2.0) + pow(start[1] - end[1], 2.0)));
 }
 
+
 int find_row (int start[], std::vector<std::vector<int>> list) {
     int row;
     for (row = 0; row < list.size(); row++){
@@ -93,13 +37,7 @@ int find_row (int start[], std::vector<std::vector<int>> list) {
     return(-1);
 }
 
-std::vector<std::vector<int>> list2matrix(std::vector<std::vector<int>> list, int col, int row) {
-    std::vector<std::vector<int>> matrix(col, std::vector<int> (row, 0));
-    for (int ind = 0; ind < list.size(); ind++) {
-        matrix[list[ind][0]][list[ind][1]] = 1;
-    }
-    return(matrix);
-}
+
 
 int find_node (int start[], std::vector<std::vector<int>> list) {
     int row;
@@ -209,34 +147,4 @@ std::vector<std::vector<int>> A_star(std::vector<std::vector<int>> map, int star
         path = pathmap;
     }
     return(path);
-}
-
-int pingpong(int current, int upper, int lower) {
-    // returns pingpong index.
-    // modulo: x % 2 = 0,1,2,0,1,2,0...for x++
-    // pingpong(x, 2) = 0,1,2,1,0,1,2... for x++
-    int mod_factor = (2 * (upper - lower) - 2);
-    int term1 = mod_factor - (current % mod_factor);
-    int term2 = current % mod_factor;
-    return (std::min(term1, term2) + lower);
-}
-
-int geometricslide(int distance, float geo_factor) {
-    // geometric slide cause the series is geometric:
-    // distance/2 + distance/4 + distance/16.... 1
-    // animates slides
-    int sign = sgn(distance);
-    int out = sign * std::max(sign * (int)(distance / geo_factor), 1);
-    return (out);
-    // std::abs() possible instead of sign*distance,
-    // but this is more elegant.
-}
-
-bool is_pressed(const Uint8 * state_array, std::vector<SDL_Scancode> to_find) {
-    for (auto it = std::begin(to_find); it != std::end(to_find); ++it) {
-        if (state_array[*it]) {
-            return (true);
-        }
-    }
-    return (false);
 }
