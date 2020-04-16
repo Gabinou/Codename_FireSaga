30d1441b54aa8a4289434e5cd1b1aea9607eefa5
Gabriel Taillon <ga.taillon@gmail.com>
2020-04-16 14:45:28 +0900

    Nevermind, keyboard controller and gamepad controller are too different.

--- include/structs.hpp
+++ include/structs.hpp
@@ -170,26 +170,6 @@ struct Settings {
 };
 
 
-struct Joystick {
-    std::vector<SDL_GameControllerAxis> mainxaxis{SDL_CONTROLLER_AXIS_LEFTX};
-    std::vector<SDL_GameControllerAxis> mainyaxis{SDL_CONTROLLER_AXIS_LEFTY};
-    std::vector<SDL_GameControllerAxis> secondxaxis{SDL_CONTROLLER_AXIS_RIGHTX};
-    std::vector<SDL_GameControllerAxis> secondyaxis{SDL_CONTROLLER_AXIS_RIGHTY};    
-}
-
-struct Triggers {
-    std::vector<SDL_GameControllerAxis> triggerleft{SDL_CONTROLLER_AXIS_TRIGGERLEFT};
-    std::vector<SDL_GameControllerAxis> triggerright{SDL_CONTROLLER_AXIS_TRIGGERRIGHT};
-}
-
-struct Dpad {
-
-}
-
-struct Buttons {
-    
-}
-
 struct KeyboardInputMap {
     std::vector<SDL_Scancode> moveright{SDL_SCANCODE_RIGHT, SDL_SCANCODE_D, SDL_SCANCODE_L};
     std::vector<SDL_Scancode> moveup{SDL_SCANCODE_W, SDL_SCANCODE_UP, SDL_SCANCODE_I};
