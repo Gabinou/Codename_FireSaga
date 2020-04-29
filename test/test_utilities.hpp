14f4f0072a62b9f61f71ab04b45a74fde1ced3d2
Gabriel Taillon <ga.taillon@gmail.com>
2020-04-29 19:26:45 +0900

    more tests.

--- test/test_utilities.hpp
+++ test/test_utilities.hpp
@@ -6,12 +6,45 @@ int upper1;
 int upper2;
 int lower;
 int out;
+int geo_factor;
+int distance;
+
 
 void test_utilities() {
     current = 0;
     upper1 = 3;
     upper2 = 6;
     lower = 0;
+    distance = 32;
+
+    distance = geometricslide(distance);
+    lok(distance == 16);
+    distance = geometricslide(distance);
+    lok(distance == 8);
+    distance = geometricslide(distance);
+    lok(distance == 4);
+    distance = geometricslide(distance);
+    lok(distance == 2);
+    distance = geometricslide(distance);
+    lok(distance == 1);
+    distance = geometricslide(distance);
+    lok(distance == 1);
+    
+    geo_factor = 3;
+    distance = 243;
+    distance = geometricslide(distance, geo_factor);
+    lok(distance == 81);
+    distance = geometricslide(distance, geo_factor);
+    lok(distance == 27);
+    distance = geometricslide(distance, geo_factor);
+    lok(distance == 9);
+    distance = geometricslide(distance, geo_factor);
+    lok(distance == 3);
+    distance = geometricslide(distance, geo_factor);
+    lok(distance == 1);
+    distance = geometricslide(distance, geo_factor);
+    lok(distance == 1);
+    
 
     out = pingpong(current, upper1);
     lok(out == 0);
