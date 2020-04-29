c45c5428e99868628f61d80f73f2272a7c1349f9
Gabriel Taillon <ga.taillon@gmail.com>
2020-04-29 19:10:47 +0900

    More pingpong tests.

--- test/test_utilities.hpp
+++ test/test_utilities.hpp
@@ -2,41 +2,57 @@
 #include "utilities.hpp"
 
 int current;
-int upper;
+int upper1;
+int upper2;
 int lower;
 int out;
 
 void test_utilities() {
     current = 0;
-    upper = 3;
+    upper1 = 3;
+    upper2 = 6;
     lower = 0;
 
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
+    lok(out == 0);
+    out = pingpong(current, upper2);
     lok(out == 0);
     
     current++;
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
+    lok(out == 1);
+    out = pingpong(current, upper2);
     lok(out == 1);
 
     current++;
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
+    lok(out == 2);
+    out = pingpong(current, upper2);
     lok(out == 2);
 
     current++;
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
     lok(out == 1);
+    out = pingpong(current, upper2);
+    lok(out == 3);
 
     current++;
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
     lok(out == 0);
+    out = pingpong(current, upper2);
+    lok(out == 4);
     
     current++;
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
     lok(out == 1);
+    out = pingpong(current, upper2);
+    lok(out == 5);
 
     current++;
-    out = pingpong(current, upper);
+    out = pingpong(current, upper1);
     lok(out == 2);
+    out = pingpong(current, upper2);
+    lok(out == 4);
     
 
 }
