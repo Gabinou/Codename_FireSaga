7d485ee04f1351a47165a85470b37c4db2058616
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-02 18:24:48 +0900

    Setting up first map tests.

--- test/test_map.hpp
+++ test/test_map.hpp
@@ -10,28 +10,32 @@ void test_map() {
     map.loadTilemap(0);
     map.setArrivals(mapArrivals[0]());
     map.setArrivalEquipments(arrivalEquipments[0]());
-    std::string asset_name;
-    entityx::Entity Uent = ex.entities.create();;
-    Unit temp_unit;
-    Unit_stats temp;
-    Inventory_item temp_wpn;
-    std::vector<short int> temp_supports;
-    Equipped temp_equipped;
-    temp = {15,  4,  5,  7,  6,   8,  4,  6,  5,  5,  6};
-    temp_unit = Unit(UNIT::NAME::SILOU, UNIT::CLASS::MAGE, temp, UNIT::SEX::F);
-    temp = {48, 14, 25, 32, 34,  28, 19, 40, 15};
-    temp_unit.setCaps(temp);
-    temp = {60, 50, 20, 60, 70,  40, 30, 20,  10, 0};
-    temp_unit.setGrowths(temp);
-    temp_unit.setBaseExp(400);
-    temp_wpn.id = ITEM::NAME::BALL_LIGHTNING;
-    temp_unit.addEquipment(temp_wpn);
+    Unit unit;
+    Unit_stats in_stats, out_stats;
+    Unit_stats in_caps, out_caps;
+    Unit_stats in_growths, out_growths;
+    Inventory_item in_wpn, out_wpn;
+    in_stats = {15,  4,  5,  7,  6,   8,  4,  6,  5,  5,  6};
+    unit = Unit(UNIT::NAME::SILOU, UNIT::CLASS::MAGE, in_stats, UNIT::SEX::F);
+    in_caps = {48, 14, 25, 32, 34, 28, 19, 40, 15};
+    unit.setCaps(in_caps);
+    in_growths = {60, 50, 20, 60, 70,  40, 30, 20,  10, 0};
+    unit.setGrowths(in_growths);
+    unit.setBaseExp(400);
+    in_wpn.id = ITEM::NAME::BALL_LIGHTNING;
+    unit.addEquipment(in_wpn);
 
-    asset_name = "..//assets//" +  temp_unit.getName() + ".png";
-    Uent.assign<Unit>(temp_unit);
+    std::string asset_name;
+    asset_name = "..//assets//" +  unit.getName() + ".png";
+    entityx::Entity Uent = ex.entities.create();;
+    Uent.assign<Unit>(unit);
     Uent.assign<Position>(6, 6);
     Uent.assign<Sprite>(asset_name.c_str());
     map.putUnit(6, 6, Uent.component<Unit>());
+
+
+
+
     map.writeXML("map_test.xml");
 
     Map test;
