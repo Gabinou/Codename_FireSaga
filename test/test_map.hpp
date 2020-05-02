c208739d71fd34ed82bac7d0e77b663dbc0d8eb8
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-02 19:57:32 +0900

    problem with map.

--- test/test_map.hpp
+++ test/test_map.hpp
@@ -75,40 +75,82 @@ void test_map() {
     std::string asset_name;
     asset_name = "..//assets//" +  unit1.getName() + ".png";
     entityx::Entity Uent1 = ex.entities.create();
-    entityx::ComponentHandle<Unit> unithandle1;
+    entityx::ComponentHandle<Unit> unithandle1, unithandle2;
     Uent1.assign<Unit>(unit1);
     Uent1.assign<Position>(6, 6);
     Uent1.assign<Sprite>(asset_name.c_str());
     map.putUnit(6, 6, Uent1.component<Unit>());
     unithandle1 = map.getUnit(6, 6);
-    out_stats = unithandle1->getStats();
 
+    if (unithandle1) {
+        out_stats = unithandle1->getStats();
+        lok(true);
+        lok(unit1.getName() == "Silou");
+        lok(unit1.getSex() == 0);
+        lok(in_stats.hp == out_stats.hp);
+        lok(in_stats.str == out_stats.str);
+        lok(in_stats.mag == out_stats.mag);
+        lok(in_stats.agi == out_stats.agi);
+        lok(in_stats.dex == out_stats.dex);
+        lok(in_stats.luck == out_stats.luck);
+        lok(in_stats.def == out_stats.def);
+        lok(in_stats.res == out_stats.res);
+        lok(in_stats.con == out_stats.con);
+        lok(in_stats.move == out_stats.move);
+        lok(in_stats.prof == out_stats.prof);
+        out_growths = unithandle1->getGrowths();
+        lok(in_growths.hp == out_growths.hp);
+        lok(in_growths.str == out_growths.str);
+        lok(in_growths.mag == out_growths.mag);
+        lok(in_growths.agi == out_growths.agi);
+        lok(in_growths.dex == out_growths.dex);
+        lok(in_growths.luck == out_growths.luck);
+        lok(in_growths.def == out_growths.def);
+        lok(in_growths.res == out_growths.res);
+        lok(in_growths.con == out_growths.con);
+        lok(in_growths.move == out_growths.move);
+        lok(in_growths.prof == out_growths.prof);
+    } else {
+        lok(false);
+    }
 
-    lok(unit1.getName() == "Silou");
-    lok(unit1.getSex() == 0);
-    lok(in_stats.hp == out_stats.hp);
-    lok(in_stats.str == out_stats.str);
-    lok(in_stats.mag == out_stats.mag);
-    lok(in_stats.agi == out_stats.agi);
-    lok(in_stats.dex == out_stats.dex);
-    lok(in_stats.luck == out_stats.luck);
-    lok(in_stats.def == out_stats.def);
-    lok(in_stats.res == out_stats.res);
-    lok(in_stats.con == out_stats.con);
-    lok(in_stats.move == out_stats.move);
-    lok(in_stats.prof == out_stats.prof);
-    out_growths = unithandle1->getGrowths();
-    lok(in_growths.hp == out_growths.hp);
-    lok(in_growths.str == out_growths.str);
-    lok(in_growths.mag == out_growths.mag);
-    lok(in_growths.agi == out_growths.agi);
-    lok(in_growths.dex == out_growths.dex);
-    lok(in_growths.luck == out_growths.luck);
-    lok(in_growths.def == out_growths.def);
-    lok(in_growths.res == out_growths.res);
-    lok(in_growths.con == out_growths.con);
-    lok(in_growths.move == out_growths.move);
-    lok(in_growths.prof == out_growths.prof);
+    map.moveUnit(6, 6, 7, 7);
+    unithandle1 = map.getUnit(6, 6);
+    unithandle2 = map.getUnit(7, 7);
+    lok(!unithandle1);
+
+    if (unithandle2) {
+        out_stats = unithandle2->getStats();
+        lok(true);
+        lok(unit1.getName() == "Silou");
+        lok(unit1.getSex() == 0);
+        lok(in_stats.hp == out_stats.hp);
+        lok(in_stats.str == out_stats.str);
+        lok(in_stats.mag == out_stats.mag);
+        lok(in_stats.agi == out_stats.agi);
+        lok(in_stats.dex == out_stats.dex);
+        lok(in_stats.luck == out_stats.luck);
+        lok(in_stats.def == out_stats.def);
+        lok(in_stats.res == out_stats.res);
+        lok(in_stats.con == out_stats.con);
+        lok(in_stats.move == out_stats.move);
+        lok(in_stats.prof == out_stats.prof);
+        out_growths = unithandle1->getGrowths();
+        lok(in_growths.hp == out_growths.hp);
+        lok(in_growths.str == out_growths.str);
+        lok(in_growths.mag == out_growths.mag);
+        lok(in_growths.agi == out_growths.agi);
+        lok(in_growths.dex == out_growths.dex);
+        lok(in_growths.luck == out_growths.luck);
+        lok(in_growths.def == out_growths.def);
+        lok(in_growths.res == out_growths.res);
+        lok(in_growths.con == out_growths.con);
+        lok(in_growths.move == out_growths.move);
+        lok(in_growths.prof == out_growths.prof);
+    } else {
+        lok(false);
+    }
+
 
     map.writeXML("map_test.xml");
 
