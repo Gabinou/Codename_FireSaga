b8b0efefe300efda33723dde3d8b057e7dbc6bb8
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-05 19:40:08 +0900

    getting rid of useless lines.

--- src/position.cpp
+++ src/position.cpp
@@ -125,7 +125,6 @@ bool Position::setPos(short int in_x, short int in_y) {
 bool Position::addPos(short int move_x, short int move_y) {
     short int newx = move_x + position[0];
     short int newy = move_y + position[1];
-    SDL_Log("to_move: %d %d\n", move_x, move_y);
     return (newPos(newx, newy));
 }
 
@@ -133,8 +132,6 @@ bool Position::newPos(short int newx, short int newy) {
     bool moved = false;
 
     if (updatable) {
-        SDL_Log("Current: %d %d\n", position[0], position[1]);
-        SDL_Log("Before: %d %d\n", newx, newy);
 
         if (periodic) {
 
@@ -149,7 +146,6 @@ bool Position::newPos(short int newx, short int newy) {
             newx = (newx - 1) % (bounds[1] - bounds[0] + 1) + bounds[0];
             newy = (newy - 1) % (bounds[3] - bounds[2] + 1) + bounds[2];
             // Python % is modulo. C/C++ % is remainder.
-            SDL_Log("After: %d %d\n", newx, newy);
         }
 
         if ((newx > bounds[0]) && (newx < bounds[1]) && (position[0] != newx)) {
