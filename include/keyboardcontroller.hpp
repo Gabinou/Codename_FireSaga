b316372d6e25dc9161582c685bd5e54623eda442
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-10 14:49:12 +0900

    deleting more useless lines from controllers.

--- include/keyboardcontroller.hpp
+++ include/keyboardcontroller.hpp
@@ -8,11 +8,8 @@ class KeyboardController {
 private:
     std::vector<std::vector<SDL_Scancode>> held_button;
     std::vector<std::vector<SDL_Scancode>> held_move;
-    // unsigned int frames_button = 0;
-    // unsigned int frames_move = 0;
     double time_button = 0.;
     double time_move = 0.;
-
     short unsigned int * tilesize;
 public:
     KeyboardController() = default;
@@ -35,24 +32,19 @@ public:
 
     void check_move(std::vector<std::vector<SDL_Scancode>>in_pressed, double dt) {
         if ((held_move == in_pressed) && (!in_pressed.empty())) {
-            // frames_move++;
             time_move += dt;
         } else {
             held_move = in_pressed;
-
-            // frames_move = 0;
             time_move = 0.;
         }
     }
 
     void check_button(std::vector<std::vector<SDL_Scancode>>in_pressed, double dt) {
         if ((held_button == in_pressed) && (!in_pressed.empty())) {
-            // frames_button++;
             time_button += dt;
         } else {
             held_button = in_pressed;
             time_button = 0.;
-            // frames_button = 0;
         }
     }
 
@@ -66,7 +58,5 @@ public:
         return (false);
     }
 };
-
-
 
 #endif /* KEYBOARDCONTROL_HPP */
