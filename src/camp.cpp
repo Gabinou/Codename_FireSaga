f972e3f3e9f903bffbeef0e482e9221b0a09e64c
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-20 00:20:54 +0900

    more progress for camp.

--- src/camp.cpp
+++ src/camp.cpp
@@ -16,50 +16,69 @@ Camp::~Camp() {
 void Camp::setChapter(char in_chapter) {
     chapter = in_chapter;
 
-    switch(chapter) {
+    switch (chapter) {
         case 0:
+        case 1:
             priority_job[UNIT::NAME::KIARA] = CAMPJOB::CLERGYMAN;
             break;
-        case 1:
-            break;
+
         case 2:
             break;
+
         case 3:
             break;
+
         case 4:
             break;
+
         case 5:
             break;
+
         case 6:
             break;
+
         case 7:
             break;
+
         case 8:
             break;
+
         case 9:
             break;
+
         case 10:
             break;
+
         case 11:
             break;
+
         case 12:
             break;
+
         case 13:
             break;
+
         case 14:
             break;
+
         case 15:
             break;
+
         case 16:
             break;
+
         case 17:
             break;
+
         case 18:
             break;
+
         case 19:
             break;
+
         case 20:
             break;
+
         case 21:
             break;
     }
