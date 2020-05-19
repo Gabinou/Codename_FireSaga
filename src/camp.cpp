859e18f429336906c61a556097080e99bde247dc
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-19 19:19:22 +0900

    camp progress.

--- src/camp.cpp
+++ src/camp.cpp
@@ -33,12 +33,12 @@ void Camp::fillJobs() {
 void Camp::makeJobNumbers() {
     party_size = party.size();
 
-    optimal_librarians = std::min(max_librarians, floor(frac_librarians * party_size));
-    optimal_cooks = std::min(max_cooks, std::max(floor(frac_cooks * party_size), 1));
-    optimal_guards = std::min(max_guards, std::max(floor(frac_guards * party_size), 1));
-    optimal_stablehands = std::min(max_stablehands, std::max(floor(frac_stablehands * party_size), 1));
-    optimal_storagemaster = std::min(max_storagemaster, std::max(floor(frac_storagemaster * party_size), 1));
-    optimal_clergymen = std::min(max_clergymen, floor(frac_clergymen * party_size));
+    optimal_librarians = std::min((unsigned char)max_librarians, (unsigned char)floor(frac_librarians * party_size));
+    optimal_cooks = std::min((unsigned char)max_cooks, (unsigned char)std::max((unsigned char)floor(frac_cooks * party_size), (unsigned char)1));
+    optimal_guards = std::min((unsigned char)max_guards, (unsigned char)std::max((unsigned char)floor(frac_guards * party_size), (unsigned char)1));
+    optimal_stablehands = std::min((unsigned char)max_stablehands, (unsigned char)std::max((unsigned char)floor(frac_stablehands * party_size), (unsigned char)1));
+    optimal_storagemaster = std::min((unsigned char)max_storagemaster, (unsigned char)std::max((unsigned char)floor(frac_storagemaster * party_size), (unsigned char)1));
+    optimal_clergymen = std::min((unsigned char)max_clergymen, (unsigned char)floor(frac_clergymen * party_size));
 
     if (game->getChapter() < 10) {
         optimal_assistant = 0;
