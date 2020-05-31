1921b90cbf3893c62d3fd10b4c00d5581f06002e
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-31 15:34:17 +0900

    I think I found a way to emit arbitrary event using template and void pointer.

--- src/menu.cpp
+++ src/menu.cpp
@@ -1,11 +1,16 @@
 
 #include "menu.hpp"
 
-void Clickable::click(entityx::Entity mouse) {
-    // switch (eventID) {
-    //     case EVENTID::UNITATTACK:
-    //         event_manager->emit<unitAttack>(mouse, unit);
-    //         break;
-    // }
-    EMIT_EVENT(unitWait);
-}
+// void Clickable::click(entityx::Entity mouse) {
+
+// void Clickable::click(entityx::Entity mouse) {
+
+// switch (eventID) {
+//     case EVENTID::UNITATTACK:
+//         event_manager->emit<unitAttack>(mouse, unit);
+//         break;
+// }
+// EMIT_EVENT(event_name.c_str());
+// event_manager->emit<event_name>(mouse, unit);
+
+// }
