aea7a0c8721b804181312520a15be9a924a40e1d
Gabriel Taillon <ga.taillon@gmail.com>
2020-05-31 14:33:00 +0900

    How to emit arbitrary event?

--- src/menu.cpp
+++ src/menu.cpp
@@ -2,9 +2,10 @@
 #include "menu.hpp"
 
 void Clickable::click(entityx::Entity mouse) {
-    switch (eventID) {
-        case EVENTID::UNITATTACK:
-            event_manager->emit<unitAttack>(mouse, unit);
-            break;
-    }
+    // switch (eventID) {
+    //     case EVENTID::UNITATTACK:
+    //         event_manager->emit<unitAttack>(mouse, unit);
+    //         break;
+    // }
+    event_manager->emit<*event>(mouse, unit)
 }
