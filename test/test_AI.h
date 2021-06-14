81868d98faba9f38a2b9cfdabc3253d8928e3001
Gabriel Taillon <ga.taillon@gmail.com>
2021-06-14 23:29:04 +0900

    tnecsing map

--- test/test_AI.h
+++ test/test_AI.h
@@ -28,53 +28,53 @@ void test_AI() {
     struct AI temp_AI2 = AI_default;
     struct AI temp_AI3 = AI_default;
 
-    ecs_world_t * world_test_ai = ecs_init();
-    ECS_COMPONENT(world_test_ai, Position);
-    ECS_COMPONENT(world_test_ai, Unit);
-    ecs_entity_t friendly_ent_1 = ecs_new(world_test_ai, 0);
-    ecs_entity_t friendly_ent_2 = ecs_new(world_test_ai, 0);
-    ecs_entity_t friendly_ent_3 = ecs_new(world_test_ai, 0);
-    ecs_entity_t enemy_ent_1 = ecs_new(world_test_ai, 0);
-    ecs_entity_t enemy_ent_2 = ecs_new(world_test_ai, 0);
-    ecs_entity_t enemy_ent_3 = ecs_new(world_test_ai, 0);
+    // ecs_world_t * world_test_ai = ecs_init();
+    // ECS_COMPONENT(world_test_ai, Position);
+    // ECS_COMPONENT(world_test_ai, Unit);
+    // ecs_entity_t friendly_ent_1 = ecs_new(world_test_ai, 0);
+    // ecs_entity_t friendly_ent_2 = ecs_new(world_test_ai, 0);
+    // ecs_entity_t friendly_ent_3 = ecs_new(world_test_ai, 0);
+    // ecs_entity_t enemy_ent_1 = ecs_new(world_test_ai, 0);
+    // ecs_entity_t enemy_ent_2 = ecs_new(world_test_ai, 0);
+    // ecs_entity_t enemy_ent_3 = ecs_new(world_test_ai, 0);
 
-    ecs_add(world_test_ai, friendly_ent_1, Position);
-    ecs_add(world_test_ai, friendly_ent_2, Position);
-    ecs_add(world_test_ai, friendly_ent_3, Position);
-    ecs_add(world_test_ai, friendly_ent_1, Unit);
-    ecs_add(world_test_ai, friendly_ent_2, Unit);
-    ecs_add(world_test_ai, friendly_ent_3, Unit);
+    // ecs_add(world_test_ai, friendly_ent_1, Position);
+    // ecs_add(world_test_ai, friendly_ent_2, Position);
+    // ecs_add(world_test_ai, friendly_ent_3, Position);
+    // ecs_add(world_test_ai, friendly_ent_1, Unit);
+    // ecs_add(world_test_ai, friendly_ent_2, Unit);
+    // ecs_add(world_test_ai, friendly_ent_3, Unit);
 
-    ecs_add(world_test_ai, enemy_ent_1, Unit);
-    ecs_add(world_test_ai, enemy_ent_2, Unit);
-    ecs_add(world_test_ai, enemy_ent_3, Unit);
-    ecs_add(world_test_ai, enemy_ent_1, Position);
-    ecs_add(world_test_ai, enemy_ent_2, Position);
-    ecs_add(world_test_ai, enemy_ent_3, Position);
+    // ecs_add(world_test_ai, enemy_ent_1, Unit);
+    // ecs_add(world_test_ai, enemy_ent_2, Unit);
+    // ecs_add(world_test_ai, enemy_ent_3, Unit);
+    // ecs_add(world_test_ai, enemy_ent_1, Position);
+    // ecs_add(world_test_ai, enemy_ent_2, Position);
+    // ecs_add(world_test_ai, enemy_ent_3, Position);
 
-    int_tile_t temp_costmapp[TEST_AI_ROW_LEN * TEST_AI_COL_LEN] = {
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-    };
+    // int_tile_t temp_costmapp[TEST_AI_ROW_LEN * TEST_AI_COL_LEN] = {
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    //     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    // };
 
 
 
