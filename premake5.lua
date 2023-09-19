ea6b2228ae564d1e76918e41cd806e5bdb6fb996
Gabriel Taillon <ga.taillon@protonmail.com>
2023-09-19 21:03:32 +0900

    [add] premake

---
+++ premake5.lua
@@ -0,0 +1,119 @@
+workspace "HelloPremake"
+	filename "HelloPremake"
+   	configurations { "Debug", "Release" } -- default is Debug
+	toolset "gcc"
+	gccprefix ""
+	language "C"
+	staticruntime "on"
+	-- cdialect "C99" -- BORKED BY STB
+
+	targetdir "A_build"
+	os.mkdir("A_build")
+	os.mkdir("B_obj")
+	objdir "!B_obj\\%{cfg.buildcfg}"
+
+	filter{"configurations:Debug"}
+		runtime "Debug"
+		kind "WindowedApp"
+		os.mkdir("B_obj\\Debug")
+        buildoptions{"-O0", "-D__SOTA_DEBUG__", "-DSDL_ASSERT_LEVEL=2 -I\\msys64\\mingw64\\include"}
+        symbols "on"
+
+	filter{"configurations:Release"}
+		runtime "Release"
+		kind "WindowedApp"
+		os.mkdir("B_obj\\Release")
+		-- optimize "on" -- what does on do -> O1?
+		buildoptions{"-O2", "-D__SOTA_RELEASE__", "-DSDL_ASSERT_LEVEL=2 -I\\msys64\\mingw64\\include"}
+
+	filter "kind:ConsoleApp"
+		files{
+			"src/*.c",
+			"src/bars/*.c",
+			"src/map/*.c",
+			"src/menu/*.c",
+			"src/popup/*.c",
+			"src/controller/*.c",
+			"src/systems/*.c",
+			"src/game/*.c",
+		}
+		includedirs{
+			".",
+			"include",
+			"include/bars",
+			"include/map",
+			"include/menu",
+			"include/popup",
+			"include/systems",
+			"include/controller",
+			"include/game",
+			"names",
+			"scenes",
+			"second_party/**",
+			"third_party/**",
+		}
+	filter {"kind:ConsoleApp", "system:Windows"}
+		links { "mingw32", "SDL2main", "SDL2", "SDL2_image", "SDL2_ttf", "glew32", "opengl32","cJSON", "nmath", "physfs", "tinymt", "tnecs", "nstr", "parg"}
+ 	filter {"kind:ConsoleApp", "system:Linux"}
+		links { "SDL2", "SDL2_image", "SDL2_ttf", "m", "GLEW", "cJSON", "nmath", "physfs", "tinymt", "tnecs", "nstr", "parg"}
+
+project "cJSON"
+	kind "StaticLib"
+	files{"third_party/cJson/**.c"}
+	includedirs{ "third_party/cJson"}
+
+project "physfs"
+	kind "StaticLib"
+	files{"third_party/physfs/**.c"}
+	includedirs{ "third_party/physfs"}
+
+project "tinymt"
+	kind "StaticLib"
+	files{"third_party/tinymt/**.c"}
+	includedirs{ "third_party/tinymt"}
+
+project "nmath"
+	kind "StaticLib"
+	basedir("second_party/noursmath")
+	files{"second_party/noursmath/**.c"}
+	includedirs{ "second_party/noursmath"}
+
+project "tnecs"
+	kind "StaticLib"
+	basedir("second_party/tnecs")
+	files{"second_party/tnecs/**.c"}
+	includedirs{ "second_party/tnecs"}
+
+project "nstr"
+	kind "StaticLib"
+	basedir("second_party/nstr")
+	files{"second_party/nstr/**.c"}
+	includedirs{ "second_party/nstr"}
+
+project "parg"
+	kind "StaticLib"
+	basedir("second_party/parg")
+	files{"second_party/parg/**.c"}
+	includedirs{ "second_party/parg"}
+
+project "sota"
+	filename "sota"
+	rules { "CustomRule" }
+	kind "ConsoleApp"
+ 	filter {"kind:ConsoleApp", "system:Linux"}
+		prebuildmessage("--- astyle formatting ---")
+		prebuildcommands {'@command -v astyle && astyle --options=utils/style.txt --verbose --recursive "src/*" "include/*" "test/*" "names/*" || true'	}
+
+project "test"
+	filename "sota_test"
+	kind "ConsoleApp"
+	files{
+		"test/**.c",
+	}
+	removefiles{"src/main.c"}
+
+rule "CustomRule"
+   fileextension ".c"
+   buildmessage "Compiling '%(Filename)'"
+   buildcommands 'gcc -c "%(FullPath)" -o "%(IntDir)/%(Filename).o"'
+   buildoutputs "%(IntDir)/%(Filename).o"
