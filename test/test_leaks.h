8746db12909a0f83e0ff642755d7ee47a525e450
Gabriel Taillon <ga.taillon@protonmail.com>
2023-10-10 09:35:52 +0900

    start test for pixels2 indices

--- test/test_leaks.h
+++ test/test_leaks.h
@@ -15,6 +15,19 @@ void test_filesystem() {
     SDL_FreeSurface(surf2);
     surf2 = NULL;
     
+    /* Filesystem_Surface_Palette_Swap leak */
+    path = PATH_JOIN("..", "assets", "Fonts", "pixelnours_test.png");
+    SDL_Surface * surf3 = Filesystem_Surface_Load(path, SDL_PIXELFORMAT_INDEX8);
+    SDL_Surface *surf4 = Filesystem_Surface_Palette_Swap(surf3, palette_NES_filter_red);
+    SDL_FreeSurface(surf3);
+    SDL_FreeSurface(surf4);
+    surf3 = NULL;
+    surf4 = NULL;
+    
+    /* Filesystem_Surface_Palette_Swap leak */
+    extern SDL_Surface *Filesystem_Surface_Pixels2Indices(SDL_Surface *r, SDL_Surface *i);
+
+
 }
 
 void test_leaks() {
