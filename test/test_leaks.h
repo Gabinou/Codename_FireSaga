18bf775d77d272599109cb994c32c9a0e6e51e3d
Gabriel Taillon <ga.taillon@protonmail.com>
2023-10-10 09:26:35 +0900

    add test filesustem

--- test/test_leaks.h
+++ test/test_leaks.h
@@ -3,9 +3,18 @@
 
 void test_filesystem() {
     SDL_Log("test_filesystem");
+   
+    /* Filesystem_indexedSurface_Init leak */
     SDL_Surface *surf = Filesystem_indexedSurface_Init(1028, 1028);
     SDL_FreeSurface(surf);
-    surf = NULL
+    surf = NULL;
+    
+    /* Filesystem_Surface_Load leak */
+    char *path = PATH_JOIN("..", "assets", "Fonts", "pixelnours_test.png");
+    SDL_Surface * surf2 = Filesystem_Surface_Load(path, SDL_PIXELFORMAT_INDEX8);
+    SDL_FreeSurface(surf2);
+    surf2 = NULL;
+    
 }
 
 void test_leaks() {
