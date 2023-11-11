e0a5b034331bc2ec988d8f40738c86c0aadd46b6
Gabriel Taillon <ga.taillon@protonmail.com>
2023-11-11 22:25:54 +0900

    [add] fth growths

--- analysis/growths.py
+++ analysis/growths.py
@@ -1,8 +1,11 @@
 
 import os
+import sys
+import json
 import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
+
 source_dir = os.path.dirname(os.path.abspath(__file__))
 
 # --- GROWTH META STATISTICAL ANALYSIS ---
@@ -23,20 +26,39 @@ for growth in growths:
 plt.legend(legends)
 # plt.show()
 plt.savefig(os.path.join(source_dir, "expected_growth.png"))
+plt.close()
 
 # --- GROWTH UNIT STATISTICAL ANALYSIS ---
+stats = ["HP", "STR", "MAG","DEX","AGI","LUCK","FTH","DEF","RES","MV","CON","PROF"]
+units = ["Erwin", "Kiara", "Silou", "Lauzon", "Pirou", "Rayan", "Sebastian", "Servil", "Perignon", "Michael"]
 
-# TODO: Total 	growth compared for all characters
-# TODO: STR 	growth compared for all characters
-# TODO: MAG 	growth compared for all characters
-# TODO: DEX 	growth compared for all characters
-# TODO: AGI 	growth compared for all characters
-# TODO: LUCK 	growth compared for all characters
-# TODO: FTH 	growth compared for all characters
-# TODO: DEF 	growth compared for all characters
-# TODO: RES 	growth compared for all characters
-# TODO: MV 		growth compared for all characters
-# TODO: CON 	growth compared for all characters
-# TODO: PROF 	growth compared for all characters
+stats_num = 12
+growths_matrix = np.array([]).reshape((0,stats_num))
+for unit in units:
+	filename = os.path.join(source_dir,"..", "units", unit + ".json")
+	with open(filename, 'r') as f:
+	    array = json.load(f)
+	    growths = array["Unit"]["Growths"]
+	growths_arr = np.array(list(growths.values())).reshape(1, -1)
+	growths_matrix = np.vstack((growths_matrix, growths_arr))
+total_growths = np.sum(growths_matrix, axis=1) 
 
+# Total growth compared for all characters
+plt.plot(np.ravel(units), np.ravel(total_growths), "ko")
+plt.ylabel("Total growths [%]")
+plt.xticks(rotation=55)
+plt.tight_layout()
+# plt.show()
+plt.savefig(os.path.join(source_dir, "units_total_growths.png"))
+plt.close()
 
+# Stat growth compared for all characters
+for i in np.arange(len(stats)):
+	stat = stats[i]
+	plt.plot(np.ravel(units), np.ravel(growths_matrix[:,i]), "ko")
+	plt.ylabel(stat + " Growths [%]")
+	plt.xticks(rotation=55)
+	plt.tight_layout()
+	# plt.show()
+	plt.savefig(os.path.join(source_dir, f"units_{stat}_growths.png"))
+	plt.close()
