48437d5c529e94a8ec116eeeeddf8c7e40b50709
Gabriel Taillon <ga.taillon@protonmail.com>
2023-11-18 22:48:32 +0900

    [clean] position

--- include/position.h
+++ include/position.h
@@ -20,8 +20,8 @@
 typedef struct Position {
     struct Point pixel_pos;
     struct Point tilemap_pos;
-    struct Point boundsmin;
-    struct Point boundsmax;
+    struct Point boundsmin; /* inclusive */
+    struct Point boundsmax; /* inclusive */
     float scale[TWO_D];
     /* If not on the Tilemap, position is absolute in window */
     bool onTilemap  : 1; /* position transformed by scale   */
@@ -29,9 +29,9 @@ typedef struct Position {
 extern struct Position Position_default;
 
 /* --- API --- */
-bool Position_Pos_Set(        struct Position *pos, i32 in_x,   i32 in_y);
-bool Position_Pos_Add(        struct Position *pos, i32 move_x, i32 move_y);
-void Position_Bounds_Set(     struct Position *pos, i32 xmin, i32 xmax, i32 ymin, i32 ymax);
+void Position_Pos_Set(   struct Position *pos, i32 in_x,   i32 in_y);
+void Position_Pos_Add(   struct Position *pos, i32 move_x, i32 move_y);
+void Position_Bounds_Set(struct Position *pos, i32 xmin, i32 xmax, i32 ymin, i32 ymax);
 void Position_replaceInbounds(struct Position *pos);
 
 #endif /* POSITION_H */
--- src/position.c
+++ src/position.c
@@ -9,19 +9,16 @@ struct Position Position_default = {
 };
 
 void Position_replaceInbounds(struct Position *spos) {
-    struct Point *pos = spos->onTilemap ? &(spos->tilemap_pos) : & (spos->pixel_pos);
-    SDL_assert(pos != NULL);
-    if (pos->x < spos->boundsmin.x)
-        pos->x = spos->boundsmin.x;
-    if (pos->x > spos->boundsmax.x)
-        pos->x = spos->boundsmax.x;
-    if (pos->y < spos->boundsmin.y)
-        pos->y = spos->boundsmin.y;
-    if (pos->y > spos->boundsmax.y)
-        pos->y = spos->boundsmax.y;
+    SDL_assert(spos != NULL);
+    struct Point *pos = spos->onTilemap ? &(spos->tilemap_pos) : &(spos->pixel_pos);
+    pos->x = (pos->x < spos->boundsmin.x) ? spos->boundsmin.x : pos->x;
+    pos->x = (pos->x > spos->boundsmax.x) ? spos->boundsmax.x : pos->x;
+    pos->y = (pos->y < spos->boundsmin.y) ? spos->boundsmin.y : pos->y;
+    pos->y = (pos->y > spos->boundsmax.y) ? spos->boundsmax.y : pos->y;
 }
 
 void Position_Bounds_Set(struct Position *spos, i32 xmin, i32 xmax, i32 ymin, i32 ymax) {
+    SDL_assert(spos != NULL);
     spos->boundsmin.x = xmin;
     spos->boundsmax.x = xmax;
     spos->boundsmin.y = ymin;
@@ -29,41 +26,16 @@ void Position_Bounds_Set(struct Position *spos, i32 xmin, i32 xmax, i32 ymin, i3
     Position_replaceInbounds(spos);
 }
 
-bool Position_Pos_Add(struct Position *spos, i32 move_x, i32 move_y) {
-    SDL_LogVerbose(SOTA_LOG_FPS, "%d %d", move_x, move_y);
-    /* used by cursor only, and cursor is basically always on the tilemap... */
-    i32 new_x, new_y;
-    if (spos->onTilemap) {
-        new_x = move_x + spos->tilemap_pos.x;
-        new_y = move_y + spos->tilemap_pos.y;
-    } else {
-        new_x = move_x + spos->pixel_pos.x;
-        new_y = move_y + spos->pixel_pos.y;
-    }
-    return (Position_Pos_Set(spos, new_x, new_y));
+void Position_Pos_Add(struct Position *spos, i32 move_x, i32 move_y) {
+    SDL_assert(spos != NULL);
+    struct Point *pos = spos->onTilemap ? &(spos->tilemap_pos) : &(spos->pixel_pos);
+    Position_Pos_Set(spos, move_x + pos->x, move_y + pos->y);
 }
 
-bool Position_Pos_Set(struct Position *spos, i32 new_x, i32 new_y) {
-    bool moved = false;
+void Position_Pos_Set(struct Position *spos, i32 new_x, i32 new_y) {
+    SDL_assert(spos != NULL);
     struct Point *pos = spos->onTilemap ? &(spos->tilemap_pos) : &(spos->pixel_pos);
-    SDL_assert(pos != NULL);
-    if ((new_x >= spos->boundsmin.x) && (new_x <= spos->boundsmax.x) && (pos->x != new_x)) {
-        pos->x = new_x;
-        moved = true;
-    } else {
-        if (new_x < spos->boundsmin.x)
-            pos->x = spos->boundsmin.x;
-        else if (new_x > spos->boundsmax.x)
-            pos->x = spos->boundsmax.x;
-    }
-    if ((new_y >= spos->boundsmin.y) && (new_y <= spos->boundsmax.y) && (pos->y != new_y)) {
-        pos->y = new_y;
-        moved = true;
-    } else {
-        if (new_y < spos->boundsmin.y)
-            pos->y = spos->boundsmin.y;
-        else if (new_y > spos->boundsmax.y)
-            pos->y = spos->boundsmax.y;
-    }
-    return (moved);
+    pos->y = new_y;
+    pos->x = new_x;
+    Position_replaceInbounds(spos);
 }
