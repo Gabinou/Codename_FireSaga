b1ba50100c2c2b6503a2758a8bed913351e54029
Gabriel Taillon <ga.taillon@protonmail.com>
2024-02-10 21:34:05 +0900

    [add] test for deployment menu

---
+++ test/menu/test_deployment.h
@@ -0,0 +1,12 @@
+#include "nourstest.h"
+#include "platform.h"
+#include "menu/growths.h"
+#include "unit/unit.h"
+#include "RNG.h"
+
+void test_menu_deployment() {
+    SDL_Log("%s " STRINGIZE(__LINE__), __func__);
+    /* --- Preliminaries --- */
+    sota_mkdir("menu_deployment");
+
+}
