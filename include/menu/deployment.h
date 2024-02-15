d052819b667962a396c68165cd8ca772f5f4e84f
Gabriel Taillon <ga.taillon@protonmail.com>
2024-02-15 23:04:46 +0900

    [fix] column alignment

--- include/menu/deployment.h
+++ include/menu/deployment.h
@@ -73,17 +73,17 @@ enum DM_MENU {
     DM_DEX_X        = DM_MAG_X  + 26,
     DM_DEX_Y        = DM_STR_Y,
 
-    DM_AGI_X        = DM_DEX_X  + 24,
+    DM_AGI_X        = DM_DEX_X  + 26,
     DM_AGI_Y        = DM_STR_Y,
 
-    DM_CON_X        = DM_AGI_X  + 24,
+    DM_CON_X        = DM_AGI_X  + 25,
     DM_CON_Y        = DM_STR_Y,
 
     /* -- Page 3 -- */
     DM_DEF_X        = DM_NAME_X + 41,
     DM_DEF_Y        = DM_NAME_Y +  4,
 
-    DM_RES_X        = DM_DEF_X  + 24,
+    DM_RES_X        = DM_DEF_X  + 25,
     DM_RES_Y        = DM_DEF_Y,
 
     DM_FTH_X        = DM_RES_X  + 26,
