be4fc812712412cb6c0f2078c8c0b7feebcabbf5
Gabriel Taillon <ga.taillon@protonmail.com>
2024-02-21 18:41:20 +0900

    [add] popup thing

--- src/game/map.c
+++ src/game/map.c
@@ -42,14 +42,16 @@ void Game_debugMap_Free(struct Game *sota) {
 void Game_debugMap_Load(struct Game *sota) {
     /* -- Preliminaries -- */
     SDL_LogDebug(SOTA_LOG_SYSTEM, "Loading in test Map\n");
+    /* - Updating game states - */
     strncpy(sota->reason, "for testing", sizeof(sota->reason));
     Game_State_Set(sota, GAME_STATE_Gameplay_Map, sota->reason);
     strncpy(sota->reason, "on Init state to GAME_STATE_Gameplay_Map substate is idle",
             sizeof(sota->reason));
+    if (game->substate != GAME_SUBSTATE_MENU)
+        Game_subState_Set(sota, GAME_SUBSTATE_MENU, sota->reason);
+
 
     Game_Map_Load(sota, CHAPTER_TEST_V7);
-
-    Game_subState_Set(sota, GAME_SUBSTATE_MENU, sota->reason);
     Game_DeploymentMenu_Enable(sota);
     Game_cursorFocus_onMenu(sota);
 
