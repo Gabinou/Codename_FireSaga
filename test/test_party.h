4554aea1e4e2a56d45734ec4760b708d0534399e
Gabriel Taillon <ga.taillon@protonmail.com>
2024-03-23 18:27:41 +0900

    [add] json commas

--- test/test_party.h
+++ test/test_party.h
@@ -1,9 +1,9 @@
 #include "unit/party.h"
 
 void test_party() {
-    s8 path_party = s8_literal("units"PHYSFS_SEPARATOR"debug_party"PHYSFS_SEPARATOR"debug.json");
+    s8 path_party = s8_literal(PATH_JOIN("units", "debug_party", "debug.json"));
 
     struct Party party = Party_default;
-    Party_Folder(&party, "units"PHYSFS_SEPARATOR"debug_party");
+    Party_Folder(&party, PATH_JOIN("units", "debug_party"));
     jsonio_readJSON(path_party, &party);
 }
