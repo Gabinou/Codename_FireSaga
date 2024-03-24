4a0ba40342544ec7c69c2f287fa528a7882cee60
Gabriel Taillon <ga.taillon@protonmail.com>
2024-03-24 14:50:28 +0900

    [update] tests

--- test/test_party.h
+++ test/test_party.h
@@ -1,10 +1,11 @@
 #include "nourstest.h"
 #include "unit/party.h"
 
-#define PARTY_FOLDER "units", "debug_party"
+#define PARTY_FOLDER "units", "test_party"
+#define DEBUG_MAP_FOLDER "units", "debug_map"
 
 void test_party() {
-    s8 path_party = s8_literal(PATH_JOIN("units", "debug_party", "debug.json"));
+    s8 path_party = s8_literal(PATH_JOIN(PARTY_FOLDER, "debug.json"));
 
     struct Party party = Party_default;
     Party_Folder(&party, PATH_JOIN(PARTY_FOLDER));
@@ -22,13 +23,13 @@ void test_party() {
 
     nourstest_true(party.filenames != NULL);
     nourstest_true(DARR_NUM(party.filenames) == 1);
-    s8 path = s8_literal(PATH_JOIN("units", "debug_map", "Boss.json"));
+    s8 path = s8_literal(PATH_JOIN(DEBUG_MAP_FOLDER, "Boss.json"));
     nourstest_true(s8equal(path, party.filenames[0]));
 
     /* --- Creating filenames for ids --- */
     Party_Ids2Filenames(&party);
     nourstest_true(DARR_NUM(party.filenames) == 2);
-    path = s8_literal(PATH_JOIN("units", "debug_map", "Boss.json"));
+    path = s8_literal(PATH_JOIN(DEBUG_MAP_FOLDER, "Boss.json"));
     nourstest_true(s8equal(path, party.filenames[0]));
     path = s8_literal(PATH_JOIN(PARTY_FOLDER, "Corsair.json"));
     nourstest_true(s8equal(path, party.filenames[1]));
@@ -36,7 +37,7 @@ void test_party() {
     /* --- Creating filenames for ids --- */
     Party_Names2Filenames(&party);
     nourstest_true(DARR_NUM(party.filenames) == 3);
-    path = s8_literal(PATH_JOIN("units", "debug_map", "Boss.json"));
+    path = s8_literal(PATH_JOIN(DEBUG_MAP_FOLDER, "Boss.json"));
     nourstest_true(s8equal(path, party.filenames[0]));
     path = s8_literal(PATH_JOIN(PARTY_FOLDER, "Corsair.json"));
     nourstest_true(s8equal(path, party.filenames[1]));
