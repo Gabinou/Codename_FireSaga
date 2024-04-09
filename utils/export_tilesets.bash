417dabf1af7da5c42869b1b621c28508009938f2
Gabriel Taillon <ga.taillon@gmail.com>
2024-04-09 21:10:47 +0900

    [add] export tilesets with aseprite, bash

---
+++ utils/export_tilesets.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Example: ls *.aseprite -1 | xargs -n1 ./export_tilesets.bash
+
+if [ -z "$1" ]
+    then
+        echo "Input one filename"
+        exit 1
+fi
+
+# Strip color from input (to use with ls *.aseprite)
+stripped=$(echo $1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
+
+# Strip extension from filename
+noext=$(echo $stripped | cut -f 1 -d ".")
+
+# Export tileset with aseprite
+echo "Exporting $stripped tileset to $noext.png"
+aseprite -b --export-tileset "$stripped" --sheet-columns 8 --sheet "$noext".png --data dump.json
--- utils/palette_switch.bash
+++ utils/palette_switch.bash
@@ -9,15 +9,15 @@ if [ -z "$1" ]
 fi
 
 # Remove color from input (to use with ls *.aseprite)
-STRIPPED=$(echo $1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
+stripped=$(echo $1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 
 # Switch color mode to RGB
-echo "Switching to RGB color mode: $STRIPPED"
-aseprite "$STRIPPED" -b --color-mode rgb --save-as "$STRIPPED"
+echo "Switching to RGB color mode: $stripped"
+aseprite "$stripped" -b --color-mode rgb --save-as "$stripped"
 
 # Change palette
-echo "Switching palette to homemade sota palette: $STRIPPED"
-aseprite "$STRIPPED" -b --palette /home/gabinours/firesaga/assets/palettes/Palette_sota.aseprite  --save-as "$STRIPPED"
+echo "Switching palette to homemade sota palette: $stripped"
+aseprite "$stripped" -b --palette /home/gabinours/firesaga/assets/palettes/Palette_sota.aseprite  --save-as "$stripped"
 
-echo "Switching to indexed color mode: $STRIPPED"
-aseprite "$STRIPPED" -b --color-mode indexed --save-as "$STRIPPED"
+echo "Switching to indexed color mode: $stripped"
+aseprite "$stripped" -b --color-mode indexed --save-as "$stripped"
