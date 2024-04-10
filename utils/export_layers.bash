7b18ab3e43e22df07734c5596017a7a598b742f4
Gabriel Taillon <ga.taillon@gmail.com>
2024-04-10 18:36:30 +0900

    [add] export layers

---
+++ utils/export_layers.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Example: ls *.aseprite -1 | xargs -n1 ./export_layers.bash
+
+if [ -z "$1" ]
+    then
+        echo "Input one filename"
+        exit 1
+fi
+
+# Remove color from input (to use with ls *.aseprite)
+stripped=$(echo $1 | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
+
+# Strip extension from filename
+noext=$(echo $stripped | cut -f 1 -d ".")
+
+# Export canvas with aseprite
+echo "Exporting $stripped canvas to $noext.png"
+aseprite -b --all-layers $stripped --oneframe --layer Font --save-as $noext".png"
