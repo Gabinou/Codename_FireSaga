8585abcb93903692bffb6488ae66378cb511b244
Gabriel Taillon <ga.taillon@gmail.com>
2024-05-11 14:14:57 +0900

    [add] rsyn utils

---
+++ utils/rsync_push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Push assets to /mnt/WDRed12
+rsync -rl --info=progress2 --delete ~/firesaga/assets /mnt/WDRed12/firesaga_assets
+# Push assets to /mnt/WDBlue1z
+rsync -rl --info=progress2 --delete ~/firesaga/assets /mnt/WDBlue1/firesaga_assets
