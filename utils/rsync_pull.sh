11c491ab26e9e006f56c227d548026fc83e53743
Gabriel Taillon <ga.taillon@gmail.com>
2024-05-12 21:05:49 +0900

    [fix] rsync stuff

--- utils/rsync_pull.sh
+++ utils/rsync_pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Push assets to /mnt/WDRed12
-rsync -rl --info=progress2 --delete /mnt/WDRed12/firesaga_assets ~/firesaga/assets
+rsync -rl --info=progress2 --delete /mnt/WDRed12/firesaga_assets/* ~/firesaga/assets
 # Push assets to /mnt/WDBlue1z
-rsync -rl --info=progress2 --delete /mnt/WDBlue1/firesaga_assets ~/firesaga/assets
+rsync -rl --info=progress2 --delete /mnt/WDBlue1/firesaga_assets/* ~/firesaga/assets
