ac64419c9b026424012ab545bf7d3d8d147a5261
Gabriel Taillon <ga.taillon@gmail.com>
2024-07-01 22:53:57 +0900

    [fix] oops oppsite

--- src/pathfinding.c
+++ src/pathfinding.c
@@ -404,9 +404,8 @@ i32 *Pathfinding_Astar_plus(i32 *path_list, i32 *costmap, tnecs_entity *occupyma
     /* [1]: http://www.redblobgames.com/pathfinding/a-star/introduction.html */
     /* -- Checks -- */
     SDL_assert((start.x != end.x) || (start.y != end.y));
-    if (costmap[start.y * col_len + start.x] >= MOVEMAP_MOVEABLEMIN) {
-        // TODO: fix?
-        // SDL_LogError(SOTA_LOG_AI, "Pathfinding: start position not moveable.");
+    if (costmap[start.y * col_len + start.x] < MOVEMAP_MOVEABLEMIN) {
+        SDL_LogError(SOTA_LOG_AI, "Pathfinding: start position not moveable.");
     }
 
     /* Alloc variables */
