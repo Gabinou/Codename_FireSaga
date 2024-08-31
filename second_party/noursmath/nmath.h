546793552f9c69cd043fe4e302d374a92e724131
Gabriel Taillon <ga.taillon@protonmail.com>
2024-08-31 21:11:56 +0900

    [add] doxygen

--- second_party/noursmath/nmath.h
+++ second_party/noursmath/nmath.h
@@ -36,28 +36,28 @@
 #define DARR_LEN(darr) (*((size_t *)darr - DARR_LEN_INDEX)) // allocated length
 #define DARR_NUM(darr) (*((size_t *)darr - DARR_NUM_INDEX)) // number of active elements
 
-// DARR_INIT: a DARR is an array with  size_t num at -1 and size_t len at -2,
+/// @brief DARR is an array with  size_t num at -1 and size_t len at -2,
 #define DARR_INIT(darr, type, len) (type*)(((size_t* )malloc(sizeof(size_t)*DARR_LEN_INDEX + sizeof(type)*(len))) + DARR_LEN_INDEX);\
     DARR_LEN(darr) = len;\
     DARR_NUM(darr) = 0;
 
-// DARR_REALLOC: DARR internal. Not to be called directly by users.
+/// @brief DARR internal. Not to be called directly by users.
 #define DARR_REALLOC(darr, len) (void *)((size_t* )realloc(((size_t* )darr - DARR_LEN_INDEX), (sizeof(size_t)*DARR_LEN_INDEX + (sizeof(*darr))*(len))) + DARR_LEN_INDEX)
 
-// DARR_GROW: increase array length by multiplying DARR_GROWTH_FACTOR
+/// @brief Increase array length by multiplying DARR_GROWTH_FACTOR
 #define DARR_GROW(darr) do {\
     DARR_LEN(darr)*=DARR_GROWTH_FACTOR;\
     darr = DARR_REALLOC(darr, DARR_LEN(darr));\
 } while(0)
 
-// DARR_PUT: put elem on top of darr, at DARR_NUM
+/// @brief Put elem on top of darr, at DARR_NUM
 #define DARR_PUT(darr, elem) do {if ((DARR_NUM(darr) + 1) >= (DARR_LEN(darr))) {\
 DARR_GROW(darr);\
 }\
 darr[DARR_NUM(darr)++] = elem;\
 } while(0)
 
-// DARR_INSERT: Put elem at index, moving other elements over
+/// @brief Put elem at index, moving other elements over
 #define DARR_INSERT(darr, elem, index) do {if ((++DARR_NUM(darr)) >= (DARR_LEN(darr))) {\
 DARR_GROW(darr);\
 }\
@@ -65,27 +65,27 @@ memmove((darr + index + 1), (darr + index), sizeof(*darr) * (DARR_NUM(darr) - in
 darr[index] = elem;\
 } while (0)
 
-// DARR_POP: get top element of darr, and "remove" it by decrementing DARR_NUM
+/// @brief get top element of darr, and "remove" it by decrementing DARR_NUM
 #define DARR_POP(darr) darr[--DARR_NUM(darr)]
 
-// DARR_DEL_SCRAMBLE: delete elem by copying top element over it, and decrementing DARR_NUM
+/// @brief Delete elem by copying top element over it, and decrementing DARR_NUM
 #define DARR_DEL_SCRAMBLE(darr, elem) do {\
     if (elem < DARR_NUM(darr)) {\
         memmove((darr + elem), (darr + --DARR_NUM(darr)), sizeof(*darr));\
     }\
 } while(0)
 
-// DARR_DEL: delete elem by moving all subsequent elements over
+/// @brief Delete elem by moving all subsequent elements over
 #define DARR_DEL(darr, elem) do {\
     if (elem < DARR_NUM(darr)) {\
         memmove((darr + elem), (darr + elem + 1), sizeof(*darr) * (--DARR_NUM(darr) - elem));\
     }\
 } while(0)
 
-// DARR_FREE: free whole darr
+/// @brief Free whole darr
 #define DARR_FREE(darr) do {free((((size_t* )darr) - DARR_LEN_INDEX));} while(0)
-
 #endif /* DARR */
+
 
 #ifndef DTAB
 #define DTAB
