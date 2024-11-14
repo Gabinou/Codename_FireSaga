e0853a35694b6048d77c32b24b38159a03d951ec
Gabriel Taillon <ga.taillon@protonmail.com>
2024-11-14 21:25:29 +0900

    [add] TODOs

--- src/input.c
+++ src/input.c
@@ -13,10 +13,9 @@ struct Input_Arguments Input_parseInputs(int argc, char *argv[]) {
     struct Input_Arguments  out_args    = Input_Arguments_default;
     struct parg_state       ps          = parg_state_default;
     int longindex[1] = {-1};
-    int c;
-    while ((c = parg_getopt_long(&ps, argc, argv, "hvS:s:m:", longopts, longindex)) != -1) {
-        // while ((c = parg_getopt_long(&ps, argc, argv, NULL, longopts, longindex)) != -1) {
-        switch (c) {
+    int shortopt;
+    while ((shortopt = parg_getopt_long(&ps, argc, argv, "hvS:s:m:", longopts, longindex)) != -1) {
+        switch (shortopt) {
             case 1:
                 printf("argument '%s' is ignored\n", ps.optarg);
                 break;
@@ -26,27 +25,29 @@ struct Input_Arguments Input_parseInputs(int argc, char *argv[]) {
                 break;
             case 'm':
                 out_args.map_index = atoi(ps.optarg);
-                // printf("out_args.map_index %d\n", out_args.map_index);
+                // TODO: Remove debug print
+                printf("out_args.map_index %d\n", out_args.map_index);
                 break;
             case 'S':
                 // Play scene with input index
                 out_args.scene = atoi(ps.optarg);
+                // TODO: Remove debug print
                 printf("out_args.scene %d\n", out_args.scene);
-                getchar();
                 break;
             case 's':
                 if (out_args.save_filename != NULL)
                     SDL_free(out_args.save_filename);
                 out_args.save_filename = (char *) SDL_malloc(strlen(ps.optarg));
                 memcpy(out_args.save_filename, ps.optarg, sizeof(*ps.optarg));
-                // printf("out_args.save_filename %s\n", out_args.save_filename);
+                // TODO: Remove debug print
+                printf("out_args.save_filename %s\n", out_args.save_filename);
                 break;
             case 'v':
                 printf("SotA version %s\n", SOTA_VER_STRING);
                 exit(0);
             case '?':
                 if (longindex[0] >= 0) {
-                    printf("option -%c/--%s requires an argument\n", longopts[longindex[0]].val,
+                    printf("option -%shortopt/--%s requires an argument\n", longopts[longindex[0]].val,
                            longopts[longindex[0]].name);
                 } else if (ps.optopt == 's') {
                     printf("option -s/--state requires an argument\n");
@@ -55,12 +56,12 @@ struct Input_Arguments Input_parseInputs(int argc, char *argv[]) {
                 } else if (ps.optopt == 'm') {
                     printf("option -m/--map requires an argument\n");
                 } else {
-                    printf("unknown option -%c\n", ps.optopt);
+                    printf("unknown option -%shortopt\n", ps.optopt);
                 }
                 exit(ERROR_Generic);
                 break;
             default:
-                printf("error: unhandled option -%c\n", c);
+                printf("error: unhandled option -%shortopt\n", shortopt);
                 exit(ERROR_Generic);
                 break;
         }
