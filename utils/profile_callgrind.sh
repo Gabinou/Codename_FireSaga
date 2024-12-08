03756675db79f3f41214bd8a8b36a78a40e6224c
Gabriel Taillon <ga.taillon@protonmail.com>
2024-12-08 17:12:10 +0900

    [add] fix profiling

--- utils/profile_callgrind.sh
+++ utils/profile_callgrind.sh
@@ -2,6 +2,6 @@
 # Written by Gabriel Taillon on August 5th 2022
 # Make a call graph, which function was called by who and how many times
 
-valgrind --tool=callgrind ~/Sync/Firesaga/A_build/CodenameFireSaga_premake
+valgrind --tool=callgrind ~/firesaga/build/sota
 gprof2dot --format=callgrind --output=out.dot "$1"
 dot -Tpng out.dot -o graph.png
--- utils/profile_perf.sh
+++ utils/profile_perf.sh
@@ -2,5 +2,6 @@
 # Written by Gabriel Taillon on August 9th 2022
 # Profiling SotA with perf
 
-sudo perf record -F 99 -g ~/firesaga/build/sota
-perf script | gprof2dot.py -f perf | dot -Tpng -o output.png
+# Need to run those commands manually for some reason...
+SDL_AUDIODRIVER=pipewire perf record -F 99 -g /home/gabinours/firesaga/build/sota
+perf script | gprof2dot -f perf | dot -Tpng -o output.png
