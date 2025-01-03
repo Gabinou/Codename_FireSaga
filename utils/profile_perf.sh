34fbaea2364abba738c5c1205834523f71aa442e
Gabriel Taillon <ga.taillon@protonmail.com>
2025-01-03 15:36:13 +0900

    [update] perf

--- utils/profile_perf.sh
+++ utils/profile_perf.sh
@@ -2,6 +2,10 @@
 # Written by Gabriel Taillon on August 9th 2022
 # Profiling SotA with perf
 
-# Need to run those commands manually for some reason...
+# Need to run those commands manually.
+# --- callgraph ---
 SDL_AUDIODRIVER=pipewire perf record -F 99 -g /home/gabinours/firesaga/build/sota
 perf script | gprof2dot -f perf | dot -Tpng -o output.png
+
+# --- cache misses ---
+perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations ./sota
