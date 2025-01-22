78b766b5d1cd1c0dc4f41ed2c93aaef509c3cad3
Gabinou <ga.taillon@protonmail.com>
2025-01-22 21:18:37 +0700

    [add] more interfaces

--- include/bars/simple.h
+++ include/bars/simple.h
@@ -18,6 +18,6 @@ enum {
 typedef struct SimpleBar StatBar;
 
 /* -- Drawing -- */
-extern const void SimpleBar_Draw(struct SimpleBar *SimpleBar, SDL_Renderer *renderer);
+extern void SimpleBar_Draw(struct SimpleBar *SimpleBar, SDL_Renderer *renderer);
 
 #endif /* SIMPLE_BARS_H */
