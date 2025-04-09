6743d8e308cc94a3a9c8fe3b79f3bb042daa806d
Gabriel Taillon <ga.taillon@protonmail.com>
2025-04-09 23:22:02 +0900

    [clean]

--- include/unit/flags.h
+++ include/unit/flags.h
@@ -8,20 +8,21 @@
 
 // TODO: toggle functions for flags
 b32 Unit_Sex(const Unit *u);
-i8  Unit_Handedness(const Unit *u);
-void Unit_Handedness_set(Unit *u, i8 handedness);
-i8  Unit_Movement(const Unit *u);
-u16 Unit_Equippable(const Unit *u);
 b32 Unit_isAlive(const Unit *u);
+i8  Unit_Movement(const Unit *u);
 b32 Unit_isWaiting(const Unit *u);
+i8  Unit_Handedness(const Unit *u);
+u16 Unit_Equippable(const Unit *u);
+b32 Unit_isTalkable(const Unit *u);
 b32 Unit_showsDanger(const Unit *u);
 b32 Unit_isDivineShield(const Unit *u);
-b32 Unit_isTalkable(const Unit *u);
 b32 Unit_isDualWielding(const Unit *u);
+
 void Unit_Alive_set(Unit *u, b32 alive);
-void Unit_DivineShield_set(Unit *u, b32 divine_shield);
 void Unit_Waiting_set(Unit *u, b32 wait);
+void Unit_Handedness_set(Unit *u, i8 handedness);
 void Unit_showsDanger_set(Unit *u, b32 show_danger);
+void Unit_DivineShield_set(Unit *u, b32 divine_shield);
 
 u64 Unit_Skills(const Unit *u);
 void Unit_Skill_Add(Unit *u, u64 skill);
