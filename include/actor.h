0e8fdb6d3dd5de4975cf9f42316b2ba96fa2c3ef
Gabriel Taillon <ga.taillon@protonmail.com>
2025-04-20 17:13:25 +0900

    [add] sactors

--- include/actor.h
+++ include/actor.h
@@ -1,3 +1,5 @@
+#ifndef ACTOR_H
+#define ACTOR_H
 
 // Actor in the theater/play sense
 // Spritesheet requirements
@@ -18,15 +20,27 @@
 //      - Frame number (blinking, lips)
 //      - Resolution of portraits, lips, eyes
 //
-// Enum example:
-//  - FACE_WIDTH    = 96; // FE8
-//  - FACE_HEIGHT   = 80; // FE8
-//  - FACE_X = 0;
-//  - LIPS_X = FACE_X + FACE_WIDTH;
-//  - EYES_X = LIPS_X + LIPS_WIDTH;
-//  - FACE_<>_Y = EXPRESSION_<> * FACE_HEIGHT;
-//  - LIPS_FRAMES_NUM = 4;
-//  - EYES_FRAMES_NUM = 3;
-//  - LIPS_<>_Y = EXPRESSION_<> * LIPS_FRAMES_NUM * LIPS_HEIGHT;
-//  - EYES_<>_Y = EXPRESSION_<> * EYES_FRAMES_NUM * EYES_HEIGHT;
 
+enum ACTOR_SPRITESHEEET {
+    /* Size of elements on spritesheet */
+    ACTOR_FACE_WIDTH    = 96, // FE8
+    ACTOR_FACE_HEIGHT   = 80, // FE8
+    ACTOR_LIPS_WIDTH    = 24,
+    ACTOR_LIPS_HEIGHT   = 24,
+    ACTOR_EYES_WIDTH    = 24,
+    ACTOR_EYES_HEIGHT   = 24,
+    ACTOR_LIPS_FRAMES   = 4,
+    ACTOR_EYES_FRAMES   = 3,
+
+    /* Position of elements columns on spritesheet */
+    ACTOR_FACE_X        = 0,
+    ACTOR_LIPS_X        = ACTOR_FACE_X + ACTOR_FACE_WIDTH,
+    ACTOR_EYES_X        = ACTOR_LIPS_X + ACTOR_LIPS_WIDTH,
+};
+
+/* Position of elements rows on spritesheet */
+i32 Actor_Face_Y(i32 face_id);
+i32 Actor_Lips_Y(i32 lips_id);
+i32 Actor_Eyes_Y(i32 eyes_id);
+
+#endif /* SCENE_H */
---
+++ src/actor.c
@@ -0,0 +1,16 @@
+#include "actor.h"
+
+
+/* Position of elements rows on spritesheet */
+i32 Actor_Face_Y(i32 face_id) {
+
+}
+
+i32 Actor_Lips_Y(i32 lips_id) {
+
+}
+
+i32 Actor_Eyes_Y(i32 eyes_id) {
+
+}
+
