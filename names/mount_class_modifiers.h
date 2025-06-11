be27bdf7b997d858bd23bc46671dddd1a191192d
Gabriel Taillon <ga.taillon@protonmail.com>
2025-06-11 22:02:21 +0900

    [add] mount class name modifiers

---
+++ names/mount_class_modifiers.h
@@ -0,0 +1,13 @@
+// Order is Mount_ID + (Male ? 0 : 1); 
+REGISTER_ENUM(Equester)         /* Magus Equester   (horse) */
+REGISTER_ENUM(Equestris)        /* Maga Equestris   (horse) */
+REGISTER_ENUM(Pegasester)       /* Magus Pegasester         */
+REGISTER_ENUM(Pegasestris)      /* Maga Pegasestris         */
+REGISTER_ENUM(Castornus)        /* Magus Castornus          */
+REGISTER_ENUM(Castorna)         /* Maga Castorna            */
+REGISTER_ENUM(Aquilister)       /* Magus Aquilister (eagle) */
+REGISTER_ENUM(Aquilistris)      /* Maga Aquilistris (eagle) */
+REGISTER_ENUM(Ansernus)         /* Magus Ansernus (goose)   */
+REGISTER_ENUM(Anserna)          /* Maga Anserna (goose)     */
+REGISTER_ENUM(Salamandrister)   /* Magus Salamandrister     */
+REGISTER_ENUM(Salamandristris)  /* Maga Salamandristris     */
