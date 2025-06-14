fc7170288ac89337c91feb3f9d6ce8132b731f0e
Gabriel Taillon <ga.taillon@protonmail.com>
2025-06-14 23:27:04 +0900

    [add] ai_icons

---
+++ names/ai_icons.h
@@ -0,0 +1,19 @@
+
+/* Charge: Moves, Attacks if in range */
+REGISTER_ENUM(CHARGE)
+/* Attack: Move & Attack if in range */
+REGISTER_ENUM(ATTACK)
+/* Guard: Attack if in range */
+REGISTER_ENUM(GUARD)    
+/* Move: to target location (+non-attack action) */
+REGISTER_ENUM(MOVE)     
+/* Flee: Move away from you (+non-attack action) */
+REGISTER_ENUM(FLEE)     
+/* Idle: Does not move (+non-attack action) */
+REGISTER_ENUM(IDLE)     
+
+/* Unknown : Enemy in range, can't read intent */
+REGISTER_ENUM(UNKNOWN)
+
+/* No icon: If enemy is out of vision range */
+REGISTER_ENUM(NO_ICON)
