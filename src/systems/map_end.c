7eb462e880ba4215103585d0df3bfcb6b41e4f87
Gabriel Taillon <ga.taillon@protonmail.com>
2025-06-20 22:10:12 +0900

    [fix] undefined

--- src/systems/map_end.c
+++ src/systems/map_end.c
@@ -4,7 +4,7 @@
 void System_Map_Repair(tnecs_input *input) {
     Inventory_item *inv_item_arr = TNECS_COMPONENT_ARRAY(input, Inventory_item_ID);
     for (size_t o = 0; o < input->num_entities; o++) {
-        Inventory_item     *inv_item = (cd_arr + o);
+        Inventory_item     *inv_item = (inv_item_arr + o);
         inv_item->used = 0;
     }
 }
