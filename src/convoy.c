c34031a72d84316ca4f88d5870a79b085af25440
Gabriel Taillon <ga.taillon@protonmail.com>
2025-07-15 22:14:13 +0900

    [clean] convoy

--- src/convoy.c
+++ src/convoy.c
@@ -144,78 +144,14 @@ Inventory_item Convoy_Item(Convoy *convoy, i32 i) {
 
 void Convoy_AllStats_Print(const Convoy *convoy,
                            i32 type_exp) {
-    // u8 start = 0, end = convoy->num_items;
-    // if (type_exp > ITEM_TYPE_EXP_NULL) {
-    //     start = convoy->cumnum[type_exp - 1];
-    //     end = convoy->cumnum[type_exp];
-    // }
-    // for (i32 i = start; i < end; i++) {
-    //     struct Inventory_item item = convoy->items[i];
-    //     struct Weapon *weapon = ((struct Weapon *)DTAB_GET_CONST(gl_weapons_dtab, item.id));
-    //     char *wpn_stats_string = Utilities_Print_wpnStats(weapon->stats);
-    //     char *item_stats_string = Utilities_Print_itemStats(weapon->item.stats);
-    //     s8 name = Item_Name(weapon->item.ids.id);
-    //     SDL_Log("%-20s \t %s %s", name.data, item_stats_string, wpn_stats_string);
-    //     SDL_free(wpn_stats_string);
-    //     SDL_free(item_stats_string);
-    // }
 }
 
 void Convoy_Stats_Print(const Convoy *convoy, i32 type_exp, i32 stattype) {
-    // SDL_assert(convoy != NULL);
-    // SDL_assert(type_exp > ITEM_TYPE_EXP_NULL);
-    // u8 start = convoy->cumnum[type_exp - 1];
-    // u8 end = convoy->cumnum[type_exp];
-    // for (i32 i = start; i < end; i++) {
-    //     struct Inventory_item item = convoy->items[i];
-    //     struct Weapon *weapon = ((struct Weapon *)DTAB_GET_CONST(gl_weapons_dtab, item.id));
-    //     s8 name = Item_Name(weapon->item.ids.id);
-    //     WeaponStatGet get = {
-    //         .stat       = stattype,
-    //         .distance   = DISTANCE_INVALID,
-    //         .hand       = WEAPON_HAND_ONE,
-    //         .infuse     = 0
-    //     };
-    //     SDL_Log("%-20s %d ", name.data, _Weapon_Stat_Raw(weapon, get));
-    // }
 }
 
 /* --- I/O --- */
 void Convoy_readJSON(void *input, const cJSON *jconvoy) {
-    // Convoy *convoy = (Convoy *) input;
-    // SDL_assert(jconvoy != NULL);
-    // cJSON *jsize    = cJSON_GetObjectItem(jconvoy, "size");
-    // cJSON *jbank    = cJSON_GetObjectItem(jconvoy, "bank");
-    // cJSON *jitems   = cJSON_GetObjectItem(jconvoy, "Items");
-    // convoy->size = cJSON_GetNumberValue(jsize);
-    // convoy->bank = cJSON_GetNumberValue(jbank);
-    // cJSON *jitem = NULL;
-    // cJSON_ArrayForEach(jitem, jitems) {
-    //     struct Inventory_item tempitem = Inventory_item_default;
-    //     Inventory_item_readJSON(&tempitem, jitem);
-    //     struct cJSON *jstored_type_exp = cJSON_GetObjectItem(jitem, "stored_type_exp");
-    //     u16 stored_type_exp = cJSON_GetNumberValue(jstored_type_exp);
-    //     Convoy_Deposit_byType(convoy, tempitem, stored_type_exp);
-    // }
 }
 
 void Convoy_writeJSON(const void *input, cJSON *jconvoy) {
-    // Convoy *convoy = (Convoy *) input;
-    // SDL_assert(jconvoy != NULL);
-    // cJSON *jsize        = cJSON_CreateNumber(convoy->size);
-    // cJSON *jbank        = cJSON_CreateNumber(convoy->bank);
-    // cJSON *jitems       = cJSON_CreateArray();
-    // cJSON *jnum_items   = cJSON_CreateNumber(convoy->num_items);
-    // cJSON_AddItemToObject(jconvoy, "size", jsize);
-    // cJSON_AddItemToObject(jconvoy, "num_items", jnum_items);
-    // cJSON_AddItemToObject(jconvoy, "bank", jbank);
-    // for (u8 i = 0; i < convoy->num_items; i++) {
-    //     cJSON *jitem = cJSON_CreateObject();
-    //     Inventory_item_writeJSON(&convoy->items[i], jitem);
-    //     u16 stored_type_exp    = Convoy_Id2TypeExp(convoy, i);
-    //     cJSON *jstored_type     = cJSON_CreateNumber(stored_type_exp);
-    //     cJSON_AddItemToObject(jitem, "stored_type_exp", jstored_type);
-    //     cJSON_AddItemToArray(jitems, jitem);
-    // }
-    // cJSON_AddItemToObject(jconvoy, "Items", jitems);
 }
