900d8ddf2c686698484267fc19a0e65da6786d80
Gabriel Taillon <ga.taillon@protonmail.com>
2026-01-16 20:54:58 +0900

    [add] clarify commands

--- macefile.c
+++ macefile.c
@@ -9,30 +9,31 @@
 #endif
 
 #ifndef BUILD_DIR
-    // TODO: central location  
+    // TODO: central location
     #define BUILD_DIR "build"
 #endif
 
 #ifndef SAVES_DIR
-    // TODO: central location  
+    // TODO: central location
     #define SAVES_DIR "saves"
 #endif
 
 #ifndef OBJ_DIR
-    // TODO: central location  
+    // TODO: central location
     #define OBJ_DIR "obj"
 #endif
 
 #ifndef INSTALL_DIR
-    // TODO: decide on good install location  
+    // TODO: decide on good install location
     #define INSTALL_DIR "install"
 #endif
 
 #ifndef EXE_NAME
+    // TODO: central location
     #define EXE_NAME sota
 #endif
 
-/* tcc doesn't support c89 */
+/* Note: tcc doesn't support c89 */
 #define C_STANDARD "-std=iso9899:1999"
 
 #define FLAGS_WARNING "-Wno-format -Wno-unused-value "\
@@ -251,7 +252,7 @@ struct Target EXE_NAME = {
     .dependencies = "zip",
 };
 
-/* Main loop for hot reloading */
+/* TODO: Main loop for hot reloading */
 struct Target sota_main = {
     .includes   = INCLUDES,
     .sources    = "src/main.c",
@@ -265,6 +266,7 @@ struct Target sota_main = {
     .kind       = MACE_EXECUTABLE,
 };
 
+/* TODO: Hot reloadable part of game */
 struct Target sota_dll = {
     .includes   = INCLUDES,
     .sources    = SOURCES,
@@ -346,49 +348,58 @@ struct Target clean = {
 };
 
 void cmd_post_cpy_bsa(Target *target) {
+    /* --- Copy archive so game can use it --- */
+
+    /* -- reading assets archive name -- */
+    /* Note: Can't comptime this include AFAIK */
 #define REGISTER_ENUM(x) char* archive = #x;
 #include "names/zip_archive.h"
 #undef REGISTER_ENUM
 
     /* Command:
-    **  1. [ -f data.bsa ] && 
-    **  2. cp -u <archive> <BUILD_DIR>/<archive> && 
-    **  3. TODO: ln -s <SAVES_DIR> <BUILD_DIR>/<SAVES_DIR> */ 
-    // Note: 3. is only for development
+    **  1. cp -u <archive> <BUILD_DIR>/<archive> */
+    /* TODO:
+    **  decide where archive BSA_DIR,
+    **  Then update command:
+    **  1. cp -u <archive> <BSA_DIR>/<archive>
+    **  2. ln -s <BSA_DIR> <BUILD_DIR>/<BSA_DIR> */
+    // Note: command_2. is only for development
 
-    char *command = calloc(1, 256);
-    char *command_1 = "cp -u ";
-    strncat(command, command_1, strlen(command_1));
-    strncat(command, archive,   strlen(archive));
-    char *command_2 = " "BUILD_DIR"/";
-    strncat(command, command_2, strlen(command_2));
-    strncat(command, archive,   strlen(archive));
-    target->cmd_post = command;
+    char *commands = calloc(1, 256);
+    char *command_1_1 = "cp -u ";
+    strncat(commands, command_1_1, strlen(command_1_1));
+    strncat(commands, archive,     strlen(archive));
+    char *command_1_2 = " "BUILD_DIR"/";
+    strncat(commands, command_1_2, strlen(command_1_2));
+    strncat(commands, archive,     strlen(archive));
+    target->cmd_post = commands;
 }
 
 void cmd_post_install(Target *target) {
-    /* -- Can't comptime this include AFAIK -- */
-    // reading assets archive name from central source
+    /* --- install game (for phony target) --- */
+
+    /* -- reading assets archive name -- */
+    /* Note: Can't comptime this include AFAIK */
 #define REGISTER_ENUM(x) char* archive = #x;
 #include "names/zip_archive.h"
 #undef REGISTER_ENUM
-    /* Command: 
+    /* Commands:
     **  1. mkdir --parents <INSTALL_DIR> &&
-    **  2. cp -u <archive> <INSTALL_DIR>/<archive> &&
+    **  2. cp -u <archive> <INSTALL_DIR>/<BSA_DIR>/<ar> &&
     **  3. cp -u build/<EXE_NAME> <INSTALL_DIR>/<EXE_NAME>
     */ 
 
-    char *command = calloc(1, 256);
+    char *commands = calloc(1, 256);
     char *command_1 = "mkdir --parents " INSTALL_DIR " && cp -u ";
-    strncat(command, command_1, strlen(command_1));
-    strncat(command, archive,   strlen(archive));
-    strncat(command, " ", 2);
+    strncat(commands, command_1, strlen(command_1));
+    strncat(commands, archive,   strlen(archive));
+    strncat(commands, " ", 2);
     char *command_2 = INSTALL_DIR "/";
-    strncat(command, command_2, strlen(command_2));
-    strncat(command, archive,   strlen(archive));
+    strncat(commands, command_2, strlen(command_2));
+    strncat(commands, archive,   strlen(archive));
     char *command_3 = " && cp -u build/" STRINGIFY(EXE_NAME) " " INSTALL_DIR "/" STRINGIFY(EXE_NAME);
-    strncat(command, command_3, strlen(command_3));
-    target->cmd_pre = command;
+    strncat(commands, command_3, strlen(command_3));
+    target->cmd_pre = commands;
 }
 
 int mace(int argc, char *argv[]) {
